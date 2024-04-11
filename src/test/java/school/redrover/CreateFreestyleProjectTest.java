cccac9b14b702ad9034dc21a85625fd03b69f2da
Viktoriia <yvika2909@gmail.com>
2024-04-11 14:51:56 +0300

    Add new update

--- src/test/java/school/redrover/CreateFreestyleProjectTest.java
+++ src/test/java/school/redrover/CreateFreestyleProjectTest.java
@@ -1,7 +1,9 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -23,4 +25,43 @@ public class CreateFreestyleProjectTest extends BaseTest {
         Assert.assertEquals(newProjectName, expectedProjectName);
 
     }
+
+    @Test
+    public void testCreatingFreestyleInvalidChar() {
+
+        String[] invalidCharacters = {"!", "@", "#", "$", "%", "^", "&", "*", "?", "|", "/", "["};
+
+        getDriver().findElement(By.xpath("//a[@it='hudson.model.Hudson@6724f5ea']")).click();
+
+        for (String invalidChar : invalidCharacters) {
+            getDriver().findElement(By.xpath("//*[@class='jenkins-input']")).clear();
+            getDriver().findElement(By.xpath("//*[@class='jenkins-input']")).sendKeys(invalidChar);
+
+            String actualResult = getDriver().findElement(By.xpath("//div[@id='itemname-invalid']"))
+                    .getText();
+            String expectedResult = "» ‘" + invalidChar + "’ is an unsafe character";
+            Assert.assertEquals(actualResult, expectedResult);
+
+            boolean okButton = getDriver().findElement(By.xpath("//button[@type='submit']")).isEnabled();
+            Assert.assertFalse(okButton);
+        }
+    }
+
+    @Test
+    public void testFreestyleProjectCreate() {
+        getDriver().findElement(By.xpath("//a[@it='hudson.model.Hudson@6724f5ea']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Project8");
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement nameOfProject = getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']"));
+
+        String actualResult = nameOfProject.getText();
+        String expectedResult = "Project8";
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
+
+
