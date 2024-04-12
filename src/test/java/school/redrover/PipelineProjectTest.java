194328c007ec1175b31262f83f2d9036e7939c2d
ElenaDetusheva <e.det@list.ru>
2024-04-12 21:12:51 +0300

    --add  enum

--- src/test/java/school/redrover/PipelineProjectTest.java
+++ src/test/java/school/redrover/PipelineProjectTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -13,14 +12,11 @@ public class PipelineProjectTest extends BaseTest {
     public void testSameNamePipeline() {
         final String PROJECT_NAME = "Random pipeline";
 
-        createNewJob(PROJECT_NAME);
-
-        getDriver().findElement(By.xpath("//*[text()='Pipeline']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+        createJob(Job.PIPELINE,PROJECT_NAME);
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
 
-        createNewJob(PROJECT_NAME);
+        goToJobPageAndEnterJobName(PROJECT_NAME);
 
         getDriver().findElement(By.xpath("//*[text()='Pipeline']")).click();
         // this line duplicates click on Pipeline, because of the Jenkins bug. Sometimes warning message doesn`t appear. Second click on Pipeline makes it happen.
@@ -55,8 +51,29 @@ public class PipelineProjectTest extends BaseTest {
 
     }
 
-    private void  createNewJob(String projectName) {
+    private void goToJobPageAndEnterJobName(String jobName) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getDriver().findElement(By.id("name")).sendKeys(jobName);
+    }
+    public void createJob(Job job, String jobName ) {
+        goToJobPageAndEnterJobName(jobName);
+        if(job==Job.PIPELINE) {
+            getDriver().findElement(By.xpath("//*[text()='Pipeline']")).click();
+        } else if (job==Job.FREESTYLE) {
+            getDriver().findElement(By.xpath("//*[text()='Freestyle project']")).click();
+        }else if (job==Job.MULTI_CONFIGURATION) {
+            getDriver().findElement(By.xpath("//*[text()='Multi-configuration project']")).click();
+        }
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    enum Job{
+        FREESTYLE,
+        PIPELINE,
+        MULTI_CONFIGURATION,
+        FOLDER,
+        MULTI_BRUNCH_PIPELINE,
+        ORGANIZATION_FOLDER
+
     }
 }
