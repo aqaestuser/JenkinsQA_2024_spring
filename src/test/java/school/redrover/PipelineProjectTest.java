584439f886a7b04b03ba9345fe8cb94ec3b200f3
xamir6 <69974022+xamir6@users.noreply.github.com>
2024-04-15 16:56:47 +0300

    RF PipelineProjectTest | Some refactoring according rules

--- src/test/java/school/redrover/PipelineProjectTest.java
+++ src/test/java/school/redrover/PipelineProjectTest.java
@@ -2,7 +2,6 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -10,16 +9,17 @@ public class PipelineProjectTest extends BaseTest {
 
     @Test
     public void testSameNamePipeline() {
+
         final String PROJECT_NAME = "Random pipeline";
 
-        createNewJob(PROJECT_NAME);
+        createNewItem(PROJECT_NAME);
 
         getDriver().findElement(By.xpath("//*[text()='Pipeline']")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
 
-        createNewJob(PROJECT_NAME);
+        createNewItem(PROJECT_NAME);
 
         getDriver().findElement(By.xpath("//*[text()='Pipeline']")).click();
         // this line duplicates click on Pipeline, because of the Jenkins bug. Sometimes warning message doesn`t appear. Second click on Pipeline makes it happen.
@@ -29,7 +29,7 @@ public class PipelineProjectTest extends BaseTest {
         Assert.assertEquals(warningMessage, "» A job already exists with the name ‘" + PROJECT_NAME + "’");
     }
 
-    private void createNewJob(String projectName) {
+    private void createNewItem(String projectName) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(projectName);
     }
