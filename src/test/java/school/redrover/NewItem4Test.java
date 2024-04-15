912b634151c3d02f1fe25860126dbcf486efb32f
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-14 23:01:46 -0700

    TC_00.004.09

---
+++ src/test/java/school/redrover/NewItem4Test.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class NewItem4Test extends BaseTest {
+    @Test
+    public void testCreateNewFolder(){
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("My folder");
+        getDriver().findElement(By.xpath("//div[contains(text(),'Creates a container')]")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//*[@name='Submit']")).click();
+
+        WebElement actualResult = getDriver().findElement(By.xpath("//div[@id='main-panel']/h1"));
+        Assert.assertEquals(actualResult.getText(), "My folder");
+    }
+}
