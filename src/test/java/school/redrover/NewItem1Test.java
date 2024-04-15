f9b36579ff46161f9ad851b96543e2d582b16497
Elena Pakhomova <140397499+pakhomova-ev@users.noreply.github.com>
2024-04-15 19:47:50 +0300

    TC_00.000.04 | New Item > Create New item > Check hint when entering an unsafe character (#709)
    * TC_00.000.04 add test

--- src/test/java/school/redrover/NewItem1Test.java
+++ src/test/java/school/redrover/NewItem1Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -10,15 +11,30 @@ public class NewItem1Test extends BaseTest {
 
     @Test
     public void testNewItemWithoutType() {
-        getDriver().findElement(
-                By.xpath("//a[@href='newJob']"))
-                .click();
-        getDriver().findElement(
-                By.cssSelector("div.add-item-name > input#name"))
-                .sendKeys("ProjectWithoutType");
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.cssSelector("div.add-item-name > input#name")).sendKeys("ProjectWithoutType");
 
-        Assert.assertFalse(getDriver().findElement(
-                By.cssSelector("div.btn-decorator > button#ok-button"))
-                .isEnabled());
+        Assert.assertFalse(getDriver().findElement(By.cssSelector("div.btn-decorator > button#ok-button")).isEnabled());
+    }
+    @Test
+    public void testTextMessageNewItemUnsafeCharInName() {
+        String unsafeSymbol = "#";
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.cssSelector("div.add-item-name > input#name")).sendKeys(unsafeSymbol);
+
+        WebElement validationMessage = getDriver().findElement(By.cssSelector("div#itemname-invalid"));
+        Assert.assertEquals((validationMessage).getText(), String.format("» ‘%s’ is an unsafe character", unsafeSymbol));
+    }
+    @Test
+    public void testColorMessageNewItemUnsafeCharInName() {
+        String unsafeSymbol = "#";
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.cssSelector("div.add-item-name > input#name")).sendKeys(unsafeSymbol);
+
+        WebElement validationMessage = getDriver().findElement(By.cssSelector("div#itemname-invalid"));
+
+        assert (Color.fromString((validationMessage).getCssValue("color")).asHex().equals("#ff0000"));
     }
 }
