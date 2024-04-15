6f7017deff62a7f9c93b615059a14b0f79b79cc9
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-15 16:11:37 -0700

    TC_00.004.09 изменение имени на NewItem6Test

---
+++ src/test/java/school/redrover/NewItem6Test.java
@@ -0,0 +1,22 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class NewItem6Test extends BaseTest {
+
+    @Test
+     public void testCreateNewFolder(){
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement (By.id("name")).sendKeys("My folder");
+        getDriver().findElement (By.xpath("//div[contains(text(), 'Creates a container')]")).click();
+        getDriver().findElement (By.id("ok-button")).click();
+        getDriver().findElement (By.xpath("//*[@name='Submit']")).click();
+
+        String actualFolderName = getDriver().findElement (By.xpath("//div[@id='main-panel']/h1")).getText();
+
+        Assert.assertEquals (actualFolderName, "My folder");
+       }
+    }
