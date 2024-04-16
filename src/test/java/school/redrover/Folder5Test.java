46008bcbc247a8cd5d59ca0991743070144bbd8e
Yuliya1985 <ulyachatutby@gmail.com>
2024-04-15 23:49:41 -0400

    TC_004.001.03 rename

---
+++ src/test/java/school/redrover/Folder5Test.java
@@ -0,0 +1,33 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class Folder5Test extends BaseTest {
+    private void createFolder(String name) {
+        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a")).click();
+        getDriver().findElement(By.xpath("//input[@id=\"name\"]")).sendKeys(name);
+        getDriver().findElement(By.xpath("//*[text()='Folder']/ancestor::li")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"ok-button\"]")).click();
+        getDriver().findElement(By.xpath("//*[@name=\"Submit\"]")).click();
+        openDashboard();
+    }
+    private void openDashboard() {
+        getDriver().findElement(By.xpath("//*[@id=\"breadcrumbs\"]/li[1]/a")).click();
+    }
+    @Test
+    public void testRenameFolder() {
+        createFolder("My new Folder");
+        getDriver().findElement(By.xpath("//*[@id=\"job_My new Folder\"]/td[3]/a/span")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[7]/span/a")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/div[1]/div[2]/input")).clear();
+        getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/div[1]/div[2]/input")).sendKeys("Folder");
+        getDriver().findElement(By.xpath("//*[@id=\"bottom-sticker\"]/div/button")).click();
+        openDashboard();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//*[@id=\"job_Folder\"]/td[3]/a/span")).isDisplayed());
+    }
+}
+
