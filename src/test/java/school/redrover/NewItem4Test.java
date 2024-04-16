25e22ad7d25b1088fab835c7e2771dfbdc70e0cb
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-15 18:13:43 -0700

    revert NewItem4Test

--- src/test/java/school/redrover/NewItem4Test.java
+++ src/test/java/school/redrover/NewItem4Test.java
@@ -1,30 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-<<<<<<< HEAD
-=======
 import org.openqa.selenium.WebElement;
->>>>>>> main
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class NewItem4Test extends BaseTest {
-<<<<<<< HEAD
-    @Test
-    public void testCreateNewFolder(){
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("My folder");
-        getDriver().findElement(By.xpath("//div[contains(text(),'Creates a container')]")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//*[@name='Submit']")).click();
-
-        String actualFolderName = getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText();
-
-        Assert.assertEquals(actualFolderName, "My folder");
-    }
-}
-=======
 
     private static final By BUTTON_NEW_ITEM = By.linkText("New Item");
 
@@ -36,5 +18,3 @@ public class NewItem4Test extends BaseTest {
         Assert.assertTrue(title.isDisplayed());
     }
 }
-
->>>>>>> main
