dbec5e08264758f51497950fef52aded707089e8
Tetiana Rakhmanova <tetiana_rakhmanova@epam.com>
2024-04-15 23:53:16 -0700

    tr_TC_00.000.19
    testGoToNewJobPage()

---
+++ src/test/java/school/redrover/NewItem7Test.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class NewItem7Test extends BaseTest {
+
+
+    @Test
+    public void testGoToNewJobPage() {
+
+        getDriver().findElement(By.linkText("New Item")).click();
+
+        String title = getDriver().findElement(By.xpath("//*[text()='Enter an item name']")).getText();
+
+        Assert.assertEquals(title, "Enter an item name");
+
+    }
+}
