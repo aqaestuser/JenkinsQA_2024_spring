7240c1c4203bf15955c7e9e2eec6b67a48edfecb
Yuliya1985 <ulyachatutby@gmail.com>
2024-04-16 09:07:32 -0400

    TC_004.001.03 made chenges

--- src/test/java/school/redrover/Folder7Test.java
+++ src/test/java/school/redrover/Folder7Test.java
@@ -1,13 +1,9 @@
 package school.redrover;
 
-
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-
-
 
 public class Folder7Test extends BaseTest {
     private void createFolder(String name) {
