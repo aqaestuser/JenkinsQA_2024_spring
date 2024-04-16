be4f6eacfccdcdb02e0a4dce218f0be68ba6f263
Yuliya1985 <ulyachatutby@gmail.com>
2024-04-16 12:23:29 -0400

    TC_004.001.03 all chenge

--- src/test/java/school/redrover/Folder6Test.java
+++ src/test/java/school/redrover/Folder6Test.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class Folder7Test extends BaseTest {
+public class Folder6Test extends BaseTest {
     private void createFolder(String name) {
         getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a")).click();
         getDriver().findElement(By.xpath("//input[@id=\"name\"]")).sendKeys(name);
