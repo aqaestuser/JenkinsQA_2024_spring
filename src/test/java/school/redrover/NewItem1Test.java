7056d24cc474debeb57c58865bc881b9cd5bc7ce
Elena Pakhomova <140397499+pakhomova-ev@users.noreply.github.com>
2024-04-17 16:32:39 +0300

    TC_00.000.17 added test displayed new job on main page (#789)
    TC_00.000.17 added test > new job displayed  on the main page

--- src/test/java/school/redrover/NewItem1Test.java
+++ src/test/java/school/redrover/NewItem1Test.java
@@ -24,6 +24,7 @@ public class NewItem1Test extends BaseTest {
         getDriver().findElement(By.cssSelector("div.add-item-name > input#name")).sendKeys(unsafeSymbol);
 
         WebElement validationMessage = getDriver().findElement(By.cssSelector("div#itemname-invalid"));
+
         Assert.assertEquals((validationMessage).getText(), String.format("» ‘%s’ is an unsafe character", unsafeSymbol));
     }
     @Test
@@ -36,5 +37,20 @@ public class NewItem1Test extends BaseTest {
         WebElement validationMessage = getDriver().findElement(By.cssSelector("div#itemname-invalid"));
 
         assert (Color.fromString((validationMessage).getCssValue("color")).asHex().equals("#ff0000"));
+    }
+    @Test
+    public void testNewItemIsDisplayedOnMainPage() {
+       final String nameProject = "firstProjectPipline";
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.cssSelector("div.add-item-name > input#name")).sendKeys(nameProject);
+        getDriver().findElement(By.cssSelector("li.org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.cssSelector("button#ok-button")).click();
+
+        getDriver().findElement(By.cssSelector("button.jenkins-button--primary ")).click();
+
+        getDriver().findElement(By.cssSelector("li.jenkins-breadcrumbs__list-item > a[href='/']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("tr#job_" + nameProject)).isDisplayed());
     }
 }
