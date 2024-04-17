b75957b5da08c3d88149b507c085cc1872e3b809
margusha83 <margusha_83@mail.ru>
2024-04-17 18:49:56 -0400

    TC_00.000.08 | Freestyle project>Create Freestyle Project

---
+++ src/test/java/school/redrover/CreateFreestyleProject2Test.java
@@ -0,0 +1,25 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class CreateFreestyleProject2Test extends BaseTest {
+
+    @Test
+    public void testCreateFreestyleProject2() {
+        final String expectedFreestyleProjectName = "FreestyleProjectTest";
+
+        getDriver().findElement(By.xpath("//div[@id='tasks']/descendant::div[1]")).click();
+        getDriver().findElement(By.id("name")).sendKeys(expectedFreestyleProjectName);
+        getDriver().findElement(By.xpath("//div[@id='items']//*[text()='Freestyle project']//ancestor::li"))
+                .click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        String expectedNameText = "FreestyleProject";
+        Assert.assertEquals(expectedNameText,expectedNameText);
+
+    }
+}
