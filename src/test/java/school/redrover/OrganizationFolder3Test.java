85f606bc43c630ef6ee33d8ca553ffeca8658322
Sveta Khudova <109797610+svetline1@users.noreply.github.com>
2024-04-17 19:29:30 +0300

    TC_00.006.07| New Item > Find Created Organization Folder on DashBoard - New test and 2 methods added (#797)

--- src/test/java/school/redrover/OrganizationFolder3Test.java
+++ src/test/java/school/redrover/OrganizationFolder3Test.java
@@ -9,6 +9,17 @@ public class OrganizationFolder3Test extends BaseTest {
 
     final String ORGANIZATION_FOLDER_NAME = "NewOrgFolder";
 
+    private void createOrganizationFolder(String folderName) {
+        getDriver().findElement(By.xpath("//a[.='New Item']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//label/span[text() ='Organization Folder']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    private void openDashboard() {
+        getDriver().findElement(By.id("jenkins-head-icon")).click();
+    }
+
     @Test
     public void testCreateOrganizationFolder(){
         getDriver().findElement(By.xpath("//a[.='New Item']")).click();
@@ -18,5 +29,14 @@ public class OrganizationFolder3Test extends BaseTest {
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), ORGANIZATION_FOLDER_NAME);
+    }
+
+    @Test
+    public void testFindOrganizationFolderOnDashboard(){
+        createOrganizationFolder(ORGANIZATION_FOLDER_NAME);
+        openDashboard();
+        String actualName = getDriver().findElement(By.linkText(ORGANIZATION_FOLDER_NAME)).getText();
+
+        Assert.assertEquals(actualName, ORGANIZATION_FOLDER_NAME);
     }
 }
