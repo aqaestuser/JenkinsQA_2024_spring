b593a0798e9dc335cd6cbf4b5dbd3da5d40abf44
Denis Prokofyev <denisprokofyev@gmail.com>
2024-04-17 12:48:42 +0300

    TC_00.004.14 | Create Folder via Create a job (#793)

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -8,6 +9,7 @@ import school.redrover.runner.BaseTest;
 public class FolderTest extends BaseTest {
 
     private static final By NAME_ERROR_MESSAGE_LOCATOR = By.id("itemname-invalid");
+    private static final String FOLDER_NAME = "First_Folder";
 
     @Test
     public void testDotAsFirstFolderNameCharErrorMessage() {
@@ -24,10 +26,23 @@ public class FolderTest extends BaseTest {
     public void testDotAsLastFolderNameCharErrorMessage() {
         getDriver().findElement(By.cssSelector("[href$='/newJob']")).click();
         getDriver().findElement(By.cssSelector("[class$='_Folder']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Folder.");
+        getDriver().findElement(By.id("name")).sendKeys("Folder." + Keys.TAB);
 
         String dotAsLastCharErrorMessage = getDriver().findElement(NAME_ERROR_MESSAGE_LOCATOR).getText();
         Assert.assertEquals(dotAsLastCharErrorMessage, "» A name cannot end with ‘.’",
                 "The error message is different");
+    }
+
+    @Test
+    public void testCreateFolderViaCreateAJob() {
+        getDriver().findElement(By.linkText("Create a job")).click();
+        getDriver().findElement(By.id("name")).sendKeys(FOLDER_NAME);
+        getDriver().findElement(By.cssSelector("[class$='_Folder']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        String breadcrumbFolderName = getDriver().findElement(By.cssSelector("[class*='breadcrumbs']>[href*='job']")).getText();
+
+        Assert.assertEquals(breadcrumbFolderName, FOLDER_NAME, "Breadcrumb name doesn't match " + FOLDER_NAME);
     }
 }
