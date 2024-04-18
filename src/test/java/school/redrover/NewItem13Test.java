61f606e7ee7fc72e30ba8c3b6280ce5f893aac89
SIA83 <140283228+SIA83@users.noreply.github.com>
2024-04-18 13:09:14 -0400

    TC_00.00.030 New item test added

---
+++ src/test/java/school/redrover/NewItem13Test.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class NewItem13Test extends BaseTest {
+
+    @Test
+    public void testCreateFreestyleProject() {
+
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys("MyNewProject");
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "MyNewProject");
+    }
+}
