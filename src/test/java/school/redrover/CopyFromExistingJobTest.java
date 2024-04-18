c29ef4c064575d91bdbc7a760e63bac0d5de6190
ElenaDetusheva <e.det@list.ru>
2024-04-18 18:07:39 +0300

    TC_00.007.03Create a new item from other existing>Check tooltip's contet

--- src/test/java/school/redrover/CopyFromExistingJobTest.java
+++ src/test/java/school/redrover/CopyFromExistingJobTest.java
@@ -1,12 +1,18 @@
 package school.redrover;
 
+import org.openqa.selenium.WebElement;
 import school.redrover.runner.BaseTest;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.TestUtils.*;
 import school.redrover.runner.TestUtils;
-    public class CopyFromExistingJobTest extends BaseTest{
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CopyFromExistingJobTest extends BaseTest{
          @Test
          public void testCopyFromExistingJob() {
              TestUtils.createNewJob(this, Job.PIPELINE, "ppp");
@@ -16,6 +22,7 @@ import school.redrover.runner.TestUtils;
              String notExistingName ="AAA";
 
              getDriver().findElement(By.cssSelector("a[href$='newJob']")).click();
+
              getDriver().findElement(By.id("name")).sendKeys("someName");
              getDriver().findElement(By.id("from")).sendKeys(notExistingName);
              getDriver().findElement(By.id("ok-button")).click();
@@ -27,4 +34,37 @@ import school.redrover.runner.TestUtils;
              Assert.assertEquals(headerError,"Error");
              Assert.assertEquals(errorText,"No such job: " + notExistingName);
          }
+
+        @Test
+        public void testDropdownMenuContent() throws InterruptedException {
+            TestUtils.createNewJob(this, Job.PIPELINE, "ppp");
+            TestUtils.createNewJob(this, Job.FREESTYLE, "folff");
+            TestUtils.createNewJob(this, Job.FOLDER, "Folder1");
+
+            String firstLetters ="foL";
+            List<WebElement> allExistingJobs = getDriver().findElements(By.cssSelector("a.jenkins-table__link.model-link.inside"));
+            List<String> allExistingJobsNames = TestUtils.getTexts(allExistingJobs);
+
+            List<String> firstLettersJobs = allExistingJobsNames
+                        .stream()
+                        .filter(el-> el.substring(0,firstLetters.length()).equalsIgnoreCase(firstLetters))
+                        .toList();
+
+            getDriver().findElement(By.cssSelector("a[href$='newJob']")).click();
+            getDriver().findElement(By.id("name")).sendKeys("someName");
+            getDriver().findElement(By.id("from")).sendKeys(firstLetters);
+
+            Thread.sleep(3000);
+
+            ArrayList<String> allJobFromThisLetterName = new ArrayList<String>();
+            List<WebElement> allJobFromThisLetter = getDriver().findElements(By.xpath("//input[@id='from']/ following-sibling::div//li"));
+
+            for (WebElement el : allJobFromThisLetter) {
+                if(!el.getText().isEmpty()){
+                    allJobFromThisLetterName.add(el.getText());
+                }
+           }
+
+            Assert.assertEquals(allJobFromThisLetterName,firstLettersJobs);
+        }
     }
