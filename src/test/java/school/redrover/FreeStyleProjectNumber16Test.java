ef15f8f76d4d7af5d080778a057cff0bca7a45c0
george.tetenkov <george.tetenkov@gmail.com>
2024-04-18 22:06:02 +0200

    Simplified the code

--- src/test/java/school/redrover/FreeStyleProjectNumber16Test.java
+++ src/test/java/school/redrover/FreeStyleProjectNumber16Test.java
@@ -11,18 +11,16 @@ public class FreeStyleProjectNumber16Test extends BaseTest {
     @Test
     public void testFreeStyleProjectExists() {
         String projectName = "New FreeStyle Project";
-        WebElement newItemButton = getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']"));
-        newItemButton.click();
-        WebElement newProjectInputField = getDriver().findElement(By.id("name"));
-        newProjectInputField.sendKeys(projectName);
-        WebElement freeStyleProjectSelector = getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']"));
-        freeStyleProjectSelector.click();
-        WebElement okButton = getDriver().findElement(By.xpath("//*[@type='submit']"));
-        okButton.click();
-        WebElement saveButton = getDriver().findElement(By.xpath("//button[contains(@class, 'jenkins-button--primary')]"));
-        saveButton.click();
-        WebElement returnToDashboad = getDriver().findElement(By.xpath("//a[text()='Dashboard']"));
-        returnToDashboad.click();
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='" + projectName + "']")).isDisplayed());
+
+        getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//*[@type='submit']")).click();
+        getDriver().findElement(By.xpath("//button[contains(@class, 'jenkins-button--primary')]")).click();
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
+
+        Assert.assertTrue(getDriver()
+                .findElement(By.xpath("//span[text()='" + projectName + "']"))
+                .isDisplayed());
     }
 }
