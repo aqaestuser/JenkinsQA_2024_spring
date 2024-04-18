8217f40889d71529266dd982a10c04e171c43688
margusha83 <margusha_83@mail.ru>
2024-04-18 15:05:55 -0400

    TC_00.000.30 | Freestyle project>Create Freestyle Project

---
+++ src/test/java/school/redrover/FreestyleProject16Test.java
@@ -0,0 +1,29 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import static org.testng.Assert.assertEquals;
+
+public class FreestyleProject16Test extends BaseTest {
+
+    @Test
+    public void testCreateFreestyleProject() {
+        final String freestyleProjectName = "FreestyleProjectTest";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(freestyleProjectName);
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']"))
+                .click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+
+        String actualFreestyleName = getDriver()
+                .findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText();
+        Assert.assertEquals(actualFreestyleName,freestyleProjectName);
+
+    }
+}
