51cc6090568dcd5f7c91f9891e7a2a6658b7f56c
Anastasia Biletska <114147000+biletskaanastasiiia@users.noreply.github.com>
2024-04-18 20:18:47 +0300

    TС_15.001.09_ManageJenkins_Nodes_CreateNewNodeWith Description (#841)
    * TС_15.001.09_ManageJenkins_Nodes_CreateNewNodeWith Description

--- src/test/java/school/redrover/Nodes2Test.java
+++ src/test/java/school/redrover/Nodes2Test.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Nodes2Test extends BaseTest {
+    public final String NODE_NAME = "New node";
+
     @Test
     public void testVerifyErrorMessageWhenCreatingNodeWithExistingNameInJenkins() throws InterruptedException {
         String nodeName = "NewNode";
@@ -31,6 +33,26 @@ public class Nodes2Test extends BaseTest {
         String actualResult = getDriver().findElement(By.xpath("//div[@class='error']")).getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testCreateNewNodeWithDescription() {
+        String description = "Description for user in node is correct and useful for next step";
+
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+        getDriver().findElement(By.xpath("//dt[text()='Nodes']")).click();
+        getDriver().findElement(By.xpath("//a[@href='new']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(NODE_NAME);
+        getDriver().findElement(By.xpath("//label[@class='jenkins-radio__label']")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.name("nodeDescription")).sendKeys(description);
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.xpath("//a[@href='../computer/" +
+                NODE_NAME.replaceAll(" ", "%20") + "/']")).click();
+
+        String actualResult = getDriver().findElement(By.id("description")).getText();
+
+        Assert.assertTrue(actualResult.contains(description));
     }
 }
 
