f9bbbbfa782c17e7b58879dae30bb807cbdf7bc7
george.tetenkov <george.tetenkov@gmail.com>
2024-04-18 20:54:46 +0200

    Frestyleproject

---
+++ src/test/java/school/redrover/FreeStyleProjectNumber16Test.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreeStyleProjectNumber16Test extends BaseTest {
+
+    @Test
+    public void testFreeStyleProjectExists() {
+        String projectName = "New FreeStyle Project";
+        WebElement newItemButton = getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']"));
+        newItemButton.click();
+        WebElement newProjectInputField = getDriver().findElement(By.id("name"));
+        newProjectInputField.sendKeys(projectName);
+        WebElement freeStyleProjectSelector = getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']"));
+        freeStyleProjectSelector.click();
+        WebElement okButton = getDriver().findElement(By.xpath("//*[@type='submit']"));
+        okButton.click();
+        WebElement saveButton = getDriver().findElement(By.xpath("//button[contains(@class, 'jenkins-button--primary')]"));
+        saveButton.click();
+        WebElement returnToDashboad = getDriver().findElement(By.xpath("//a[text()='Dashboard']"));
+        returnToDashboad.click();
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='" + projectName + "']")).isDisplayed());
+    }
+}
