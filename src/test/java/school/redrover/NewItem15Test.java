87bac758ef009a9004eb8d1d691d8d9ee044bdda
AndreySereda <andreysereda77@gmail.com>
2024-04-18 22:43:45 +0200

    create TC_00.000.29

--- src/test/java/school/redrover/NewItem15Test.java
+++ src/test/java/school/redrover/NewItem15Test.java
@@ -6,17 +6,15 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class NewItem15Test extends BaseTest {
+
     @Test
-    public void testNewItem15Test(){
-        getDriver().findElement(By.cssSelector("#tasks > div:nth-child(1)> span > a")).click();
+    public void testNewFolderCreating(){
+        getDriver().findElement(By.cssSelector("a[href$=\"/newJob\"]")).click();
         getDriver().findElement(By.cssSelector("#name")).sendKeys("New Folder");
-        getDriver().findElement(By.xpath("//*[@id=\"j-add-item-type-nested-projects\"]/ul/li[1]")).click();
+        getDriver().findElement(By.xpath("//li[contains(@class, '_Folder')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/div[2]/div/div[2]/input"))
-                .sendKeys("Example");
         getDriver().findElement(By.name("Submit")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(), "Example");
-
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("h1")).getText(), "New Folder");
     }
 }
