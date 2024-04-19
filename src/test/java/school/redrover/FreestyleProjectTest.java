ca59731b4e6ec084a38f73df8474198e20f15b76
AlekseyS <115942572+AlekseySGH@users.noreply.github.com>
2024-04-19 16:48:05 +0300

    TS_00.007.05 New item Create a new item from other existing (#837)
    * TS_00.007.05 New item Create a new item from other existing

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -1,14 +1,23 @@
 package school.redrover;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.*;
 import org.testng.annotations.*;
 import school.redrover.runner.*;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FreestyleProjectTest extends BaseTest {
     private static final String FREESTYLE_PROJECT_NAME = "Freestyle Project Name";
     private static final String NEW_FREESTYLE_PROJECT_NAME = "New Freestyle Project Name";
+
+    private WebElement okButton() {
+        return getDriver().findElement(By.id("ok-button"));
+    }
 
     private WebElement submitButton() {
         return getDriver().findElement(By.xpath("//button[@name = 'Submit']"));
@@ -19,20 +28,30 @@ public class FreestyleProjectTest extends BaseTest {
     }
 
     public void freestyleProjectCreate(String newName) {
+        WebDriverWait wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(newName);
-        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+        wait5.until(ExpectedConditions.elementToBeClickable(getDriver().findElement(
+                By.className("hudson_model_FreeStyleProject")))).click();
+        wait5.until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.id("ok-button")))).click();
         submitButton().click();
     }
 
     public void createFolder(String folderName) {
-        getDriver().findElement(By.xpath("//a [@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys(folderName);
-        getDriver().findElement(By.xpath("//span [@class='label'] [text() = 'Folder']")).click();
-        getDriver().findElement(By.xpath("//button [@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//span[@class='label'][text() = 'Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         submitButton().click();
+    }
 
+    public void createNewItemFromOtherExisting(String newProjectName, String existingProjectName) {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(newProjectName);
+        getDriver().findElement(By.xpath("//input[@name='from']")).sendKeys(existingProjectName);
+        okButton().click();
+        submitButton().click();
     }
 
     public void openElementDropdown(WebElement element) {
@@ -164,7 +183,7 @@ public class FreestyleProjectTest extends BaseTest {
         getDriver().findElement(By.id("jenkins-home-link")).click();
 
         WebElement projectName = getDriver().findElement(
-                By.xpath("//span[text()='"+ FREESTYLE_PROJECT_NAME +"']/following-sibling::button[@class='jenkins-menu-dropdown-chevron']"));
+                By.xpath("//span[text()='" + FREESTYLE_PROJECT_NAME + "']/following-sibling::button[@class='jenkins-menu-dropdown-chevron']"));
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));", projectName);
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('click'));", projectName);
 
@@ -192,17 +211,17 @@ public class FreestyleProjectTest extends BaseTest {
         jenkinsHomeLink().click();
 
         openElementDropdown(getDriver().findElement(
-                By.xpath("//a [@href='job/" + projectName.replaceAll(" ", "%20")
-                        + "/']/button [@class='jenkins-menu-dropdown-chevron']")));
+                By.xpath("//a[@href='job/" + projectName.replaceAll(" ", "%20")
+                        + "/']/button[@class='jenkins-menu-dropdown-chevron']")));
 
-        getDriver().findElement(By.xpath("//a [@href='/job/"
+        getDriver().findElement(By.xpath("//a[@href='/job/"
                 + projectName.replaceAll(" ", "%20") + "/move']")).click();
 
-        getDriver().findElement(By.xpath("//option [@value='/" + folderName + "']")).click();
+        getDriver().findElement(By.xpath("//option[@value='/" + folderName + "']")).click();
 
         submitButton().click();
 
-        String actualResult = getDriver().findElement(By.xpath("//div [@id='main-panel']")).getText();
+        String actualResult = getDriver().findElement(By.xpath("//div[@id='main-panel']")).getText();
 
         Assert.assertTrue(actualResult.contains(expectedResult));
     }
@@ -214,7 +233,7 @@ public class FreestyleProjectTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//a[@data-build-success='Build scheduled']")).click();
         String actualResult = getDriver().findElement(By.xpath("//*[@href='/job/"
-                                + FREESTYLE_PROJECT_NAME.replaceAll(" ", "%20") + "/1/']")).getText();
+                + FREESTYLE_PROJECT_NAME.replaceAll(" ", "%20") + "/1/']")).getText();
 
         Assert.assertEquals(actualResult, "#1");
     }
@@ -233,4 +252,34 @@ public class FreestyleProjectTest extends BaseTest {
 
     }
 
+    @Test
+    public void testCopyFromContainer() {
+
+        String oldProjectName1 = "Race Cars";
+        String oldProjectName2 = "Race Bikes";
+        String newProjectName = "Vintage Cars";
+
+        freestyleProjectCreate(oldProjectName1);
+        jenkinsHomeLink().click();
+
+        freestyleProjectCreate(oldProjectName2);
+        jenkinsHomeLink().click();
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(newProjectName);
+        getDriver().findElement(
+                By.xpath("//input[@name='from']")).sendKeys(oldProjectName1.substring(0, 1));
+
+        WebDriverWait wait20 = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+
+        List<WebElement> elements = wait20.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+                By.xpath("//div[@class='item-copy']//li[not(@style='display: none;')]")));
+
+        List<String> elementsList = new ArrayList<>();
+        for (WebElement element : elements) {
+            elementsList.add(element.getText());
+        }
+
+        Assert.assertTrue(elementsList.contains(oldProjectName1));
+    }
 }
