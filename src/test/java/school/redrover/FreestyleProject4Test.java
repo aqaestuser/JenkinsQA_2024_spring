01a92ae1e4276793e516d7e3d27ff69e4aa8741d
Alex Ronin <sir.nevajn@yandex.ru>
2024-04-20 17:44:32 +0300

    RF _00.000.01 (#899)
    2.4 waiters in the first test were added

--- src/test/java/school/redrover/FreestyleProject4Test.java
+++ src/test/java/school/redrover/FreestyleProject4Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -9,28 +10,29 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject4Test extends BaseTest {
 
-    @Ignore
     @Test
     public void testCreateNewFreestyleProject() {
 
         final String projectItemName = "JavaHashGroupProject";
 
-        WebElement newItemButton = getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']"));
+        WebElement newItemButton = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@href='/view/all/newJob']")));
         newItemButton.click();
 
-        WebElement inputNameField = getDriver().findElement(By.xpath("//input[@name='name']"));
+        WebElement inputNameField = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='name']")));
         inputNameField.sendKeys(projectItemName);
 
-        WebElement freestyleProjectButton = getDriver().findElement(By.xpath("//span[contains(text(),'Freestyle project')]"));
+        WebElement freestyleProjectButton = getWait2().until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Freestyle project')]")));
         freestyleProjectButton.click();
 
-        WebElement okButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
+        WebElement okButton = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
         okButton.click();
 
-        WebElement saveButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        WebElement saveButton = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@name='Submit']")));
         saveButton.click();
 
-        WebElement newProjectHeader = getDriver().findElement(By.xpath("//div[@class='jenkins-app-bar__content jenkins-build-caption']"));
+        WebElement newProjectHeader = getWait2().until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-app-bar__content jenkins-build-caption']")));
 
         Assert.assertTrue(newProjectHeader.isDisplayed());
         Assert.assertEquals(newProjectHeader.getText(),projectItemName);
