6c392cececae9069c0ad741c3dd90ef33662fcb8
kubanbekoff <159750066+kubanbekoff@users.noreply.github.com>
2024-04-20 19:41:01 +0600

    delete method, change locators on 18, 24 lines. (#895)

--- src/test/java/school/redrover/FreestyleProject8Test.java
+++ src/test/java/school/redrover/FreestyleProject8Test.java
@@ -2,41 +2,31 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FreestyleProject8Test extends BaseTest {
-
-    public void createFreestyleProject(){
+    @Test
+    public void testCreateProject(){
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys("Freestyle project");
-        getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//*[contains(@class,'FreeStyleProject')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
+        getWait10();
+        Assert.assertEquals(getDriver().findElement(By.xpath(
+                        "//*[@id='main-panel']/div[1]/div/h1"))
+                .getText(), "Freestyle project");
     }
 
-    @Ignore
-    @Test
-    public void testCreateFreestyleProject(){
-        getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Freestyle project");
-        getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Freestyle project");
-    }
-
-    @Test
+    @Test(dependsOnMethods = "testCreateProject")
     public void testRenamingFreestyleProject(){
-        createFreestyleProject();
-        getDriver().findElement(By.xpath("//*[contains(@href, 'rename')]")).click();
+        getDriver().findElement(By.xpath("//*[@href='job/Freestyle%20project/']/span")).click();
+        getDriver().findElement(By.xpath("//*[@id='tasks']/div[7]/span/a")).click();
         getDriver().findElement(By.xpath("//*[@name='newName']")).clear();
         getDriver().findElement(By.xpath("//*[@name='newName']")).sendKeys("New name");
         getDriver().findElement(By.xpath("//*[@name = 'Submit']")).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "New name");
     }
-
 }
