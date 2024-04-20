8b5e0a9f3fe8472831cd583aebe287b77ce286ad
Yunus Saliiev <120329741+SudUnix@users.noreply.github.com>
2024-04-19 22:21:18 -0400

    Ys/tc 00.001.24 create freestyle project (#865)
    * TC_00.001.24 | Create Freestyle Project
    
    * TC_00.001.24 | Create Freestyle Project
    
    * TC_00.001.24 | Create Freestyle Project
    
    * TC_00.001.24 | Create Freestyle Project

--- src/test/java/school/redrover/FreestyleProject17Test.java
+++ src/test/java/school/redrover/FreestyleProject17Test.java
@@ -1,24 +1,26 @@
 package school.redrover;
 
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class FreestyleProject17Test extends BaseTest{
-    private static final String PROJECT_NAME = "FreestyleProject Name";
+
+public class FreestyleProject17Test extends BaseTest {
+
+    private final String JOB_NAME = "YS_jenkins_ui";
 
     @Test
-    public void testCreateNewFreestyleProject() {
-        getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(PROJECT_NAME);
-        getDriver().findElement(By.xpath("//span[contains(text(),'Freestyle project')]")).click();
-        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+    void createNewFreestyleProjectTest() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(JOB_NAME);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("[type='submit']")).click();
+        getDriver().findElement(By.cssSelector("[name='description']"))
+                .sendKeys("This test is trying to create a new freestyle job");
+        getDriver().findElement(By.cssSelector("[name='Submit']")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']/div/div/h1")).getText(), "FreestyleProject Name");
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), JOB_NAME);
     }
-
 }
-
-
