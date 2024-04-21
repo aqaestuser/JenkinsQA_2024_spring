2ea46f6170704518bedc62a03cb7f1aa67129463
Helen Muronyuk <olena6623@gmail.com>
2024-04-21 15:06:12 +0200

    TC_02.004.08 | Pipeline > Pipeline Configuration for the existing Pipeline > Check discard old builds per count

--- src/test/java/school/redrover/PipelineConfigurationTest.java
+++ src/test/java/school/redrover/PipelineConfigurationTest.java
@@ -1,15 +1,19 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class PipelineConfigurationTest extends BaseTest {
+    private static final String JOB_NAME = "TestCrazyTesters";
     public void createPipline() {
         getDriver().findElement(By.xpath("//span[contains(text(),'Create')]")).click();
-        getDriver().findElement(By.id("name")).sendKeys("TestCrazyTesters");
+        getDriver().findElement(By.id("name")).sendKeys(JOB_NAME);
         getDriver().findElement(By.xpath("//li[contains(@class,'WorkflowJob')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
     }
@@ -48,5 +52,28 @@ public class PipelineConfigurationTest extends BaseTest {
 
         Assert.assertTrue(
                 getDriver().findElement(By.id("enable-project")).getText().contains(expectedMessageForDisabledProject));
+    }
+
+    @Test
+    public void testDiscardOldBuildsByCount() {
+        createPipline();
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text() = 'Discard old builds']"))).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name = '_.numToKeepStr']"))).sendKeys("1");
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-section-id='pipeline']"))).click();
+        WebElement sampleScript = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'samples']//select")));
+        Select sampleScriptSelect = new Select(sampleScript);
+        sampleScriptSelect.selectByValue("hello");
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@name = 'Submit']"))).click();
+
+        WebElement buildButton = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-build-success = 'Build scheduled']")));
+        buildButton.click();
+        buildButton.click();
+        getWait5().until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//div[@id = 'buildHistoryPage']//tr"), 1));
+        getWait2().until(ExpectedConditions.invisibilityOfAllElements(getDriver().findElements(By.xpath("//td[contains(@class, 'progress-bar')]"))));
+        getDriver().navigate().refresh();
+        WebElement secondBuild = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@class = 'build-row-cell']//a[text() = '#2']")));
+
+        Assert.assertTrue(secondBuild.getAttribute("href").contains("/job/" + JOB_NAME.replaceAll(" ", "%20") + "/2/"), "there is no second build");
     }
 }
