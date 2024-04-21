924c9843e32464dc2b311f790c0491699a8e605e
Lyubov Ivanova <77409288+l-ivanova@users.noreply.github.com>
2024-04-21 18:19:08 +0300

    RF _00.000.36 | New item > Create New item with unsafe characters in name (#925)
    Co-authored-by: l.ivanova <l.ivanova@a1qa.com>

--- src/test/java/school/redrover/NewItem16Test.java
+++ src/test/java/school/redrover/NewItem16Test.java
@@ -3,14 +3,12 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import java.time.Duration;
 
-public class NewItemValidationTest extends BaseTest {
+public class NewItem16Test extends BaseTest {
 
     @DataProvider(name = "unsafeCharacters")
     public Object [][] unsafeCharacters() {
@@ -22,11 +20,9 @@ public class NewItemValidationTest extends BaseTest {
 
     @Test(dataProvider = "unsafeCharacters")
     public void testCreateNewItemWithUnsafeCharacterInName(String x) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(x);
-        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("itemname-invalid")));
+        WebElement errorMessage = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("itemname-invalid")));
 
         Assert.assertEquals(errorMessage.getText(),"» ‘" + x + "’ is an unsafe character");
         Assert.assertEquals(errorMessage.getCssValue("color"), "rgba(255, 0, 0, 1)");
