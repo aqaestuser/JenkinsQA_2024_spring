41366d58b9486d4a0a3e6135364f268a73efef05
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-04-22 12:55:36 +0300

    TС_09.005.01 | Manage Jenkins > Appearance > Verify quantity of Themes (#900)
    * TС_09.005.01 | Manage Jenkins > Appearance > Verify quantity of Themes

---
+++ src/test/java/school/redrover/ManageAppearanceTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class ManageAppearanceTest extends BaseTest{
+
+        @Test
+        public void testAppearanceQuantityOfThemesViaDashboardDropDown() {
+            WebElement dashboard = getDriver().findElement(By.cssSelector("div#breadcrumbBar a[href = '/']"));
+            WebElement chevron = dashboard.findElement(By.cssSelector("[class$='chevron']"));
+
+            ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));", chevron);
+            ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('click'));", chevron);
+
+            getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='tippy-box'] [href='/manage']"))).click();
+            getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
+
+            Assert.assertEquals(getDriver().findElements(By.className("app-theme-picker__item")).size(), 3);
+        }
+    }
