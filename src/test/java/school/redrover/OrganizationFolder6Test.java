96a3804bad392573a91e281ecd98c3447e89642e
anastasiia148 <anastasiia.shelest1996@gmail.com>
2024-04-22 16:09:10 -0400

    TC_00.006.03 add updated test case, create organization folder

--- src/test/java/school/redrover/OrganizationFolder6Test.java
+++ src/test/java/school/redrover/OrganizationFolder6Test.java
@@ -9,16 +9,17 @@ public class OrganizationFolder6Test extends BaseTest {
 
     @Test
     public void testCreateOrganization6Folder() {
-        getDriver().findElement(By.xpath("//a[@it='hudson.model.Hudson@41cfd365']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("organization folder");
+        final String orgFolderName = "organization folder";
+        getDriver().findElement(By.xpath("//a[@class='task-link task-link-no-confirm '][@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(orgFolderName);
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.className("jenkins-breadcrumbs__list-item")).click();
 
         String actualCreatedFolder = getDriver().findElement(By.xpath(
-                "//tr[@id='job_organization folder']//a[@class='jenkins-table__link model-link inside']")).getText();
-        Assert.assertEquals(actualCreatedFolder, "organization folder");
+                "//tr[@id='job_" + orgFolderName +"']//a[@class='jenkins-table__link model-link inside']")).getText();
+        Assert.assertEquals(actualCreatedFolder, orgFolderName);
     }
 }
 
