4431a4373d5891ad9069bb666eb38e0d2f73bfff
Natalia <natalia.mylova@gmail.com>
2024-04-23 18:11:51 +0100

    RF_00.004.11 | Rename, Add wait

--- src/test/java/school/redrover/CreateFolder2Test.java
+++ src/test/java/school/redrover/CreateFolder2Test.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -8,17 +9,15 @@ import school.redrover.runner.BaseTest;
 
 public class CreateFolder2Test extends BaseTest {
 
-    @Ignore
     @Test
     public void testCreateNewFolder() {
         getDriver().findElement(By.xpath("//a[.='New Item']")).click();
-        getDriver().findElement(By.id("name")).click();
         getDriver().findElement(By.id("name")).sendKeys("New Folder");
         getDriver().findElement(By.xpath("//label[.='Folder']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
-        getDriver().findElement(By.xpath("//a[.='Dashboard']")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[.='New Folder']")).isDisplayed());
+        getWait2().until(ExpectedConditions.presenceOfElementLocated(By.name("Submit"))).click();
+
+        Assert.assertTrue(getDriver().findElement(By.tagName("h1")).isDisplayed());
     }
 }
