da18054440384f2fb08e7f40255d1ef7f3df2fa2
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-04-23 12:55:57 +0300

    RF | Item > Add reset theme after method (#973)

--- src/test/java/school/redrover/ManageAppearance0Test.java
+++ src/test/java/school/redrover/ManageAppearance0Test.java
@@ -5,52 +5,62 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class ManageAppearance0Test extends BaseTest{
+public class ManageAppearance0Test extends BaseTest {
 
     void goToManageAppearance() {
         getDriver().findElement(By.linkText("Manage Jenkins")).click();
         getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
     }
 
-        @Test
-        public void testAppearanceQuantityOfThemesViaDashboardDropDown() {
-            WebElement dashboard = getDriver().findElement(By.cssSelector("div#breadcrumbBar a[href = '/']"));
-            WebElement chevron = dashboard.findElement(By.cssSelector("[class$='chevron']"));
+    @Test
+    public void testAppearanceQuantityOfThemesViaDashboardDropDown() {
+        WebElement dashboard = getDriver().findElement(By.cssSelector("div#breadcrumbBar a[href = '/']"));
+        WebElement chevron = dashboard.findElement(By.cssSelector("[class$='chevron']"));
 
-            ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));", chevron);
-            ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('click'));", chevron);
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));", chevron);
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].dispatchEvent(new Event('click'));", chevron);
 
-            getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='tippy-box'] [href='/manage']"))).click();
-            getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='tippy-box'] [href='/manage']"))).click();
+        getDriver().findElement(By.cssSelector("[href=\"appearance\"]")).click();
 
-            Assert.assertEquals(getDriver().findElements(By.className("app-theme-picker__item")).size(), 3);
-        }
+        Assert.assertEquals(getDriver().findElements(By.className("app-theme-picker__item")).size(), 3);
+    }
 
     @Test
-        public void testDarkThemeSwitchNotification() {
+    public void testDarkThemeSwitchNotification() {
+        goToManageAppearance();
+
+        getDriver().findElement(By.cssSelector("[for='radio-block-0']")).click();
+        getDriver().findElement(By.name("Apply")).click();
+
+        Assert.assertEquals(
+                getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("notification-bar"))).getText(),
+                "Saved");
+    }
+
+    @Test
+    public void testDarkThemeSwitchColor() {
+        goToManageAppearance();
+
+        getDriver().findElement(By.cssSelector("[for='radio-block-0']")).click();
+        getDriver().findElement(By.name("Apply")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.tagName("body")).getCssValue("background-color"),
+                "rgba(31, 31, 35, 1)",
+                "The background color doesn't match the theme");
+    }
+
+    @AfterMethod
+    public void returnToNoneTheme() {
+        if (!getDriver().findElement(By.tagName("html")).getAttribute("data-theme").equals("none")) {
             goToManageAppearance();
-
-            getDriver().findElement(By.cssSelector("[for='radio-block-0']")).click();
+            getDriver().findElement(By.cssSelector("[for='radio-block-2']")).click();
             getDriver().findElement(By.name("Apply")).click();
-
-            Assert.assertEquals(
-                    getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("notification-bar"))).getText(),
-                    "Saved");
-        }
-
-        @Test
-        public void testDarkThemeSwitchColor() {
-            goToManageAppearance();
-
-            getDriver().findElement(By.cssSelector("[for='radio-block-0']")).click();
-            getDriver().findElement(By.name("Apply")).click();
-
-            Assert.assertEquals(
-                    getDriver().findElement(By.tagName("body")).getCssValue("background-color"),
-                    "rgba(31, 31, 35, 1)",
-                    "The background color doesn't match the theme");
         }
     }
+}
