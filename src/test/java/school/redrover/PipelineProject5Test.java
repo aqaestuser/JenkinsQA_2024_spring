fb8585f00e29d618f854d9f25cbf3ec174ae6382
olganik08 <66400202+olganik08@users.noreply.github.com>
2024-04-23 12:11:22 -0700

    TC_00.002.07 (#988)

---
+++ src/test/java/school/redrover/PipelineProject5Test.java
@@ -0,0 +1,24 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class PipelineProject5Test extends BaseTest {
+    private static final String PROJECT_NAME = "NewPipeline";
+    @Test
+    public void createProject(){
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'jenkins-app-bar__content jenkins-build-caption']/h1")).getText(),
+                PROJECT_NAME);
+
+    }
+
+
+}
