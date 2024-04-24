27b1ae370d32fd99a7910cdaa1dbaf248b651b6e
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-23 20:59:40 -0700

    TC_00.004.18, TC_00.004.09, TC_04.001.11 - refactoring

--- src/test/java/school/redrover/NewItem6Test.java
+++ src/test/java/school/redrover/NewItem6Test.java
@@ -7,8 +7,8 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class NewItem6Test extends BaseTest {
-     String folderName = "MyFolder";
-     String newFolderName = "Your name";
+    private final String FOLDER_NAME = "MyFolder";
+    private final String NEW_FOLDER_NAME = "Your name";
 
     @Test
     public void testCreateNewFolder(){
@@ -16,54 +16,44 @@ public class NewItem6Test extends BaseTest {
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
 
-        getDriver().findElement(By.id("name")).sendKeys(folderName);
+        getDriver().findElement(By.id("name")).sendKeys(FOLDER_NAME);
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Creates a container')]")));
 
         getDriver().findElement(By.xpath("//div[contains(text(),'Creates a container')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='Submit']")));
-
-        getDriver().findElement(By.xpath("//*[@name='Submit']")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='Submit']"))).click();
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='main-panel']/h1")));
 
         String actualFolderName = getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText();
 
-        Assert.assertEquals(actualFolderName, folderName);
+        Assert.assertEquals(actualFolderName, FOLDER_NAME);
     }
 
     @Test(dependsOnMethods = "testCreateNewFolder")
     public void testNewFolderOnDashboard(){
         getDriver().findElement(By.xpath("//li[@class='jenkins-breadcrumbs__list-item']")).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='jenkins-table__link model-link inside']")));
+        String actualNewFolderName = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='jenkins-table__link model-link inside']"))).getText();
 
-        String actualNewFolderName = getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText();
-
-        Assert.assertEquals(actualNewFolderName,folderName );
+        Assert.assertEquals(actualNewFolderName,FOLDER_NAME);
     }
 
     @Test(dependsOnMethods = "testNewFolderOnDashboard")
     public void testRenameFolder(){
         getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[@class='task-link-wrapper '])[7]")));
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='tasks']//*[contains(@href,'confirm-rename')]"))).click();
 
-        getDriver().findElement(By.xpath("(//span[@class='task-link-wrapper '])[7]")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.name("newName"))).clear();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.name("newName")));
-
-        getDriver().findElement(By.name("newName")).clear();
-
-        getDriver().findElement(By.name("newName")).sendKeys(newFolderName);
+        getDriver().findElement(By.name("newName")).sendKeys(NEW_FOLDER_NAME);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
+        String actualFolderNewName =  getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1"))).getText();
 
-        String actualFolderNewName = getDriver().findElement(By.cssSelector("h1")).getText();
-
-        Assert.assertEquals(actualFolderNewName,newFolderName);
+        Assert.assertEquals(actualFolderNewName,NEW_FOLDER_NAME);
     }
 }
