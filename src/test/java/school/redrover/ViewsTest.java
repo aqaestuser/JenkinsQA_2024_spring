06f67ba8ccbfb17cd60ba6bf0fe52392be90f446
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-04-24 14:48:28 +0300

    TC_10.001.02 | Views > Create "View" > Verify opening "My View" via the user dropdown menu (#1007)

--- src/test/java/school/redrover/ViewsTest.java
+++ src/test/java/school/redrover/ViewsTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,5 +16,18 @@ public class ViewsTest extends BaseTest {
         TestUtils.openElementDropdown(this, userElement);
         getDriver().findElement(By.cssSelector("div.tippy-box [href $= 'my-views']")).click();
         Assert.assertTrue(getWait10().until(ExpectedConditions.urlContains("my-views/view/all")));
+    }
+
+    @Test
+    public void testGoToMyViewFromUsernameDropdown() {
+        new Actions(getDriver())
+                .moveToElement(getDriver().findElement(By.cssSelector("[data-href$='admin']")))
+                .pause(1000)
+                .click()
+                .perform();
+
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[href$='admin/my-views']"))).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("[href$='my-views/']")).isDisplayed());
     }
 }
