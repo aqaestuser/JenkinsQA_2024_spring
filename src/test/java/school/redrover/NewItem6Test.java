be41a454ecf4190c4dcd76ddd45dfdb5d576b938
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-23 17:36:00 -0700

    TC_00.004.18, TC_00.004.09, TC_04.001.11

--- src/test/java/school/redrover/NewItem6Test.java
+++ src/test/java/school/redrover/NewItem6Test.java
@@ -1,14 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class NewItem6Test extends BaseTest {
+     String folderName = "MyFolder";
+     String newFolderName = "Your name";
 
     @Test
     public void testCreateNewFolder(){
@@ -16,7 +16,7 @@ public class NewItem6Test extends BaseTest {
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
 
-        getDriver().findElement(By.id("name")).sendKeys("My folder");
+        getDriver().findElement(By.id("name")).sendKeys(folderName);
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Creates a container')]")));
 
@@ -31,41 +31,39 @@ public class NewItem6Test extends BaseTest {
 
         String actualFolderName = getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText();
 
-        Assert.assertEquals(actualFolderName, "My folder");
+        Assert.assertEquals(actualFolderName, folderName);
     }
 
     @Test(dependsOnMethods = "testCreateNewFolder")
     public void testNewFolderOnDashboard(){
         getDriver().findElement(By.xpath("//li[@class='jenkins-breadcrumbs__list-item']")).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'My folder')]")));
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='jenkins-table__link model-link inside']")));
 
-        String actualNewFolderName = getDriver().findElement(By.xpath("//span[contains(text(),'My folder')]")).getText();
+        String actualNewFolderName = getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText();
 
-        Assert.assertEquals(actualNewFolderName,"My folder" );
- }
+        Assert.assertEquals(actualNewFolderName,folderName );
+    }
 
     @Test(dependsOnMethods = "testNewFolderOnDashboard")
     public void testRenameFolder(){
-        WebElement actualNewFolderName = getDriver().findElement(By.xpath("//span[contains(text(),'My folder')]"));
+        getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).click();
 
-        Actions actions = new Actions(getDriver());
-        actions.moveToElement(actualNewFolderName).perform();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[@class='task-link-wrapper '])[7]")));
 
-        getDriver().findElement(By.xpath("(//button[@class='jenkins-menu-dropdown-chevron'])[3]")).click();
+        getDriver().findElement(By.xpath("(//span[@class='task-link-wrapper '])[7]")).click();
 
-        getDriver().findElement(By.xpath("//a[@href='/job/My%20folder/confirm-rename']")).click();
-
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.name("newName")));
 
         getDriver().findElement(By.name("newName")).clear();
-        getDriver().findElement(By.name("newName")).sendKeys("Your folder");
+
+        getDriver().findElement(By.name("newName")).sendKeys(newFolderName);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
 
         String actualFolderNewName = getDriver().findElement(By.cssSelector("h1")).getText();
 
-        Assert.assertEquals(actualFolderNewName,"Your folder");
+        Assert.assertEquals(actualFolderNewName,newFolderName);
     }
 }
