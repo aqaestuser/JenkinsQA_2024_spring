d84f1c5d873ff09bf5d0908360486333a921931a
Ildana <ildana.frolova@yandex.ru>
2024-04-24 20:28:41 -0400

    full stage view test

---
+++ src/test/java/school/redrover/PipelineProject6Test.java
@@ -0,0 +1,50 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class PipelineProject6Test extends BaseTest {
+    private static final String PIPELINE_NAME = "Pipeline";
+    private Actions actions;
+    public Actions getActions() {
+        if (actions==null){
+            actions=new Actions(getDriver());
+        }
+        return actions;
+    }
+    public void createNewPipeline(String pipelineName){
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/view/all/newJob']"))).click();
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.id("name"))).sendKeys(pipelineName);
+        getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='Submit']"))).click();
+    }
+    public void goHomePage(){
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@class='jenkins-breadcrumbs__list-item']"))).click();
+    }
+
+    @Test
+    public void testFullStageViewDropDownMenu(){
+        createNewPipeline(PIPELINE_NAME);
+        goHomePage();
+
+        WebElement dropDown = getDriver().findElement(By.xpath("//tr[@id='job_" + PIPELINE_NAME + "']//a[@href='job/" + PIPELINE_NAME + "/']"));
+        getActions().moveToElement(dropDown).perform();
+
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']//button"))).click();
+
+        getWait10().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//a[@href='/job/" + PIPELINE_NAME + "/workflow-stage']"))).click();
+
+        String expectedText = PIPELINE_NAME + " - Stage View";
+        Assert.assertEquals(getWait5().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@id='pipeline-box']/h2"))).getText(),expectedText);
+    }
+}
