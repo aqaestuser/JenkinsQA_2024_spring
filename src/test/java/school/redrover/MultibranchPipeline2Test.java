779c65b74f9ea965aa527aefd0a1c3e3734d6369
Ilya T <IlyaT123@yandex.ru>
2024-04-25 15:34:02 +0300

    //

--- src/test/java/school/redrover/MultibranchPipeline2Test.java
+++ src/test/java/school/redrover/MultibranchPipeline2Test.java
@@ -5,10 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
-
-import static school.redrover.runner.TestUtils.MULTIBRANCH_PIPELINE;
-
 
 public class MultibranchPipeline2Test extends BaseTest {
 
