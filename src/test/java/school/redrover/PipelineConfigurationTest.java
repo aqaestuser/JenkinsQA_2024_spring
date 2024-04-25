128b27b374179fd106ad7bd49b40e86cb5d2f979
kas-113 <148212061+kas-113@users.noreply.github.com>
2024-04-25 19:23:52 +0200

    TC_02.004.13 | Pipeline > Pipeline Configuration for the existing Pipeline > Add Display Name in Advanced Project Options section (#1038)

--- src/test/java/school/redrover/PipelineConfigurationTest.java
+++ src/test/java/school/redrover/PipelineConfigurationTest.java
@@ -1,13 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -34,6 +34,7 @@ public class PipelineConfigurationTest extends BaseTest {
         getDriver().findElement(By.xpath("//span[contains(text(),'Create')]")).click();
         getDriver().findElement(By.id("name")).sendKeys(JOB_NAME);
         getDriver().findElement(By.xpath("//li[contains(@class,'WorkflowJob')]")).click();
+        getWait60().until(ExpectedConditions.visibilityOfElementLocated(By.id("ok-button")));
         getDriver().findElement(By.id("ok-button")).click();
     }
 
@@ -152,5 +153,32 @@ public class PipelineConfigurationTest extends BaseTest {
 
         Assert.assertTrue(getDriver().findElement(By.xpath("//div[text()='PROJECT']")).isDisplayed(),
                 "PROJECT");
+    }
+
+    @Test
+    public void testAddDisplayNameInAdvancedSection() {
+        final String displayNameText = "This is project's Display name text for Advanced Project Options";
+        final By advancedButton = By.xpath("//section[@class='jenkins-section']//button[@type='button']");
+
+        createPipeline();
+
+        navigateToConfigurePageFromDashboard();
+
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[@data-section-id='advanced-project-options']"))).click();
+
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].dispatchEvent(new Event('click'));",
+                getDriver().findElement(advancedButton));
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("//div[@class='setting-main']//input[contains(@checkurl, 'checkDisplayName')]")))
+                .sendKeys(displayNameText);
+
+        getDriver().findElement(SAVE_BUTTON_CONFIGURATION).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']")).getText(),
+                displayNameText);
     }
 }
