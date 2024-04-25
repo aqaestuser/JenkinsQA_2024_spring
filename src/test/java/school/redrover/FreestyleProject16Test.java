e1ad5f9ca0a0c00c531b728c147ab32a705d24fb
Maminer151 <manunka151@gmail.com>
2024-04-25 16:19:44 +0200

    TC_01.002.16 | Freestyle project > Rename project

--- src/test/java/school/redrover/FreestyleProject16Test.java
+++ src/test/java/school/redrover/FreestyleProject16Test.java
@@ -6,8 +6,10 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+
 public class FreestyleProject16Test extends BaseTest {
     final String PROJECT_NAME = "MD first project";
+    final String NEW_PROJECT_NAME = "Rename MD first project";
 
     @Ignore
     @Test
@@ -32,6 +34,28 @@ public class FreestyleProject16Test extends BaseTest {
         Assert.assertEquals(general, "General");
     }
 
+    @Test
+    public void testRenameFirstProject(){
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.xpath
+                ("//*[@id='breadcrumbs']/li[1]")).click();
+        getDriver().findElement(By.xpath
+                ("//span[text()='" + PROJECT_NAME + "']")).click();
+        getDriver().findElement(By.xpath
+                ("//*[@id='tasks']/div[7]/span/a")).click();
+        getDriver().findElement(By.name("newName")).clear();
+        getDriver().findElement(By.name("newName")).sendKeys(NEW_PROJECT_NAME);
+        getDriver().findElement(By.name("Submit")).click();
+        String newProjectName = getDriver().findElement(By.tagName("h1")).getText();
+
+        Assert.assertEquals(newProjectName, NEW_PROJECT_NAME);
+    }
+
+    @Ignore
     @Test
     public void testCreateFreestyleProject() {
         final String freestyleProjectName = "FreestyleProjectTest";
