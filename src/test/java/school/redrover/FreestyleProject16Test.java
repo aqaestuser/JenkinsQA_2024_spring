c532d4a21ff0c7ec129cab9fc993c6ef5e6adf70
Maminer151 <manunka151@gmail.com>
2024-04-26 18:48:06 +0200

    1. TC_01.001.16 | Freestyle project > Add description 2. US_01.004 | Freestyle project > Delete project

--- src/test/java/school/redrover/FreestyleProject16Test.java
+++ src/test/java/school/redrover/FreestyleProject16Test.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -9,6 +10,7 @@ import school.redrover.runner.BaseTest;
 public class FreestyleProject16Test extends BaseTest {
     final String PROJECT_NAME = "MD first project";
     final String NEW_PROJECT_NAME = "Rename MD first project";
+    final String PROJECT_DESCRIPTION = "I like my project!";
 
 
     @Test
@@ -48,6 +50,40 @@ public class FreestyleProject16Test extends BaseTest {
         String newProjectName = getDriver().findElement(By.tagName("h1")).getText();
 
         Assert.assertEquals(newProjectName, NEW_PROJECT_NAME);
+    }
+
+    @Test (dependsOnMethods = "testCreateFirstTest")
+    public void testAddDescription(){
+
+        getDriver().findElement(By.xpath
+                ("//span[text()='" + PROJECT_NAME + "']")).click();
+        getDriver().findElement(By.xpath("//*[@id='description-link']")).click();
+        getDriver().findElement(By.name
+                ("description")).sendKeys(PROJECT_DESCRIPTION);
+        getDriver().findElement(By.name("Submit")).click();
+
+        String description = getDriver().findElement
+                (By.xpath("//*[@id='description']/div[1]")).getText();
+
+        Assert.assertEquals(description, PROJECT_DESCRIPTION);
+    }
+
+
+    @Test (dependsOnMethods = "testCreateFirstTest")
+    public void testDeleteFirstProject(){
+
+        getDriver().findElement(By.xpath
+                ("//span[text()='" + PROJECT_NAME + "']")).click();
+        getDriver().findElement(By.xpath
+                ("//span[text()='Delete Project']")).click();
+        getDriver().findElement(By.xpath
+                ("//*[@id='jenkins']/dialog/div[3]/button[1]")).click();
+        getDriver().findElement(By.id("search-box")).sendKeys(PROJECT_NAME, Keys.RETURN);
+
+        String not_found = getDriver().findElement(By.xpath
+                ("//*[@id='main-panel']/div")).getText();
+
+        Assert.assertEquals(not_found, "Nothing seems to match.");
     }
 
 
