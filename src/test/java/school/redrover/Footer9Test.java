938cc0b96d42b4499e58abc7eb5c952e63e643d1
Ilya T <IlyaT123@yandex.ru>
2024-04-26 13:21:07 +0300

    created Footer9Test class with 3 tests

--- src/test/java/school/redrover/Footer9Test.java
+++ src/test/java/school/redrover/Footer9Test.java
@@ -6,37 +6,44 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import java.util.List;
 
+import java.util.List;
 
 
 public class Footer9Test extends BaseTest {
 
     @Test
-    public void testJenkinsInformationVersionFooter() {
+    public void testJenkinsVersion() {
+        getDriver().findElement(By.cssSelector("[class$='jenkins_ver']")).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.xpath("//a[@href='/manage/about']")))).click();
 
-        List<String> checkЕabBar = List.of("Mavenized dependencies", "Static resources", "License and dependency information for plugins");
+        Assert.assertTrue(getDriver().findElement(By.xpath("//p[.='Version 2.440.2']")).isDisplayed());
+    }
 
+    @Test
+    public void testDropDownLink() {
         getDriver().findElement(By.cssSelector("[class$='jenkins_ver']")).click();
 
         WebElement dropDownMenu = getDriver().findElement(By.cssSelector("#tippy-1"));
         WebElement aboutJenkins = getWait5().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.xpath("//a[@href='/manage/about']"))));
+
         Assert.assertTrue(aboutJenkins.isDisplayed());
+        Assert.assertTrue(dropDownMenu.findElement(By.xpath("//a[@href='https://www.jenkins.io/participate/']")).isDisplayed());
+        Assert.assertTrue(dropDownMenu.findElement(By.xpath("//a[@href='https://www.jenkins.io/']")).isDisplayed());
+    }
 
-        WebElement getInvolved = dropDownMenu.findElement(By.xpath("//a[@href='https://www.jenkins.io/participate/']"));
-        Assert.assertTrue(getInvolved.isDisplayed());
+    @Test
+    public void testJenkinsInformationFooter() {
+        List<String> tabBarMenu = List.of("Mavenized dependencies", "Static resources", "License and dependency information for plugins");
 
-        WebElement webSite = dropDownMenu.findElement(By.xpath("//a[@href='https://www.jenkins.io/']"));
-        Assert.assertTrue(webSite.isDisplayed());
+        getDriver().findElement(By.cssSelector("[class$='jenkins_ver']")).click();
 
-        getDriver().findElement(By.xpath("//a[@href='/manage/about']")).click();
-
-        Assert.assertTrue(getDriver().findElement(By.xpath("//p[.='Version 2.440.2']")).isDisplayed());
+        getWait5().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.xpath("//a[@href='/manage/about']")))).click();
 
         WebElement tabBar = getDriver().findElement(By.xpath("//div[@class='tabBar']"));
 
-        Assert.assertTrue(tabBar.getText().contains(checkЕabBar.get(0)), "Mavenized dependencies");
-        Assert.assertTrue(tabBar.getText().contains(checkЕabBar.get(1)), "Static resources");
-        Assert.assertTrue(tabBar.getText().contains(checkЕabBar.get(2)), "License and dependency information for plugins");
+        Assert.assertTrue(tabBar.getText().contains(tabBarMenu.get(0)), "Expected: " + tabBarMenu.get(0));
+        Assert.assertTrue(tabBar.getText().contains(tabBarMenu.get(1)), "Expected: " + tabBarMenu.get(1));
+        Assert.assertTrue(tabBar.getText().contains(tabBarMenu.get(2)), "Expected: " + tabBarMenu.get(2));
     }
 }
