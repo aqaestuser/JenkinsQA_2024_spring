2db6d8ed6c685b280d32e1b37d32d913c32699bf
Anzhelika1202 <kulyani.74@gmail.com>
2024-04-25 19:55:43 -0700

    TC_14.006.02

---
+++ src/test/java/school/redrover/HeaderSearchHelpTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class HeaderSearchHelpTest extends BaseTest {
+
+    @Test
+    public void testAccessToUserDoc(){
+        getDriver().findElement(By.xpath("//input[@id='search-box']"));
+        getDriver().findElement(By.xpath("//a[@class='main-search__icon-trailing']")).click();
+
+        String actualHandbook = getWait10().until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='sidebar-content']/h5"))).getText();
+        String actualTutorials = getDriver().findElement(By.xpath("(//div[@id='sidebar-content']/h5)[2]")).getText();
+        String actualResources = getDriver().findElement(By.xpath("(//div[@id='sidebar-content']/h5)[3]")).getText();
+
+        Assert.assertEquals(actualHandbook, "User Handbook");
+        Assert.assertEquals(actualTutorials, "Tutorials");
+        Assert.assertEquals(actualResources, "Resources");
+    }
+}
+
