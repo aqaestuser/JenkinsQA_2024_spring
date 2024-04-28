f7e2067ef20438054e5784ee5127cd9659e642c3
Dr.Johanson <doc.johanson@gmail.com>
2024-04-28 20:30:57 +0500

    Add @Ignore testAddDescription

--- src/test/java/school/redrover/EditFolderDescriptionAndDeleteTest.java
+++ src/test/java/school/redrover/EditFolderDescriptionAndDeleteTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -32,6 +33,7 @@ public class EditFolderDescriptionAndDeleteTest extends BaseTest {
         getDriver().findElement(By.id("jenkins-name-icon")).click();
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testAddFolder")
     public void testAddDescription() {
         getDriver().findElement(
