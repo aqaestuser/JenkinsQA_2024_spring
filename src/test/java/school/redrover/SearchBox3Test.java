24e34c5c969b17c074c3edff6dd59bdbd75779db
Evgenia Lysenko <kirzhoy@ya.ru>
2024-04-29 12:44:00 +0400

    remove test to common class

--- src/test/java/school/redrover/SearchBox3Test.java
+++ src/test/java/school/redrover/SearchBox3Test.java
@@ -1,12 +1,14 @@
 package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class SearchBox3Test extends BaseTest {
     private static final By SEARCH_BOX = By.xpath("//input[@id='search-box']");
+    private static final By SYSTEM_PAGE = By.xpath("//h1[.='System']");
 
     @Test
     public void testSearchWithValidData() {
@@ -16,5 +18,15 @@ public class SearchBox3Test extends BaseTest {
         String systemPageTitle = getDriver().findElement(By.tagName("h1")).getText();
 
         Assert.assertEquals(systemPageTitle, "System");
+    }
+
+    @Test
+    public void testSearchUsingSuggestList() {
+       getDriver().findElement(SEARCH_BOX).sendKeys("c");
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[@class='yui-ac-bd']//ul//li[.='config']"))).click();
+        getDriver().findElement(SEARCH_BOX).sendKeys(Keys.ENTER);
+
+        Assert.assertTrue(getDriver().findElement(SYSTEM_PAGE).isDisplayed());
     }
 }
