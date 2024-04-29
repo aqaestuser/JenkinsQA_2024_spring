281315293cf04e2b79553224697f5914c070d2b5
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-04-29 10:38:48 +0300

    RF | Delete Ignore (#1133)

--- src/test/java/school/redrover/UserTest.java
+++ src/test/java/school/redrover/UserTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -35,7 +34,6 @@ public class UserTest extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
     }
 
-    @Ignore
     @Test
     public void testUsersSortingByName() {
 
