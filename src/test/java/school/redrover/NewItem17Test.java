1fd40472da31dd57d96a3cd336d5cad7b837e04e
Dmitriy Grishin <grishindu@gmail.com>
2024-04-29 21:47:27 -0500

    Added new Assertion for configuration page, Added new test for TC_00.001.30 | New Item > Create Freestyle Project > TestUtils

--- src/test/java/school/redrover/NewItem17Test.java
+++ src/test/java/school/redrover/NewItem17Test.java
@@ -2,31 +2,39 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import school.redrover.runner.TestUtils;
+
 public class NewItem17Test extends BaseTest {
+
+    private static final String PROJECT_NAME = "FreeStyleProjectDG";
 
     @Test
     public void testCreateNewItemFP () {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("TestItemFor");
+        getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
         getDriver().findElement(By.xpath("//li[contains(@class, '_FreeStyleProject')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
         String configurationHeaderH1 = getDriver().findElement(By.tagName("h1")).getText();
-        Assert.assertEquals( configurationHeaderH1, "Configure");
+        Assert.assertEquals(configurationHeaderH1, "Configure");
 
         String configurationPageBreadcrumbs = getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]")).getText();
         Assert.assertEquals(configurationPageBreadcrumbs, "Configuration");
-    }
 
-    @Ignore
-    @Test (dependsOnMethods = "testCreateNewItemFP")
-    public void testConfigurationPage () {
         String sourceCodeManagementBlock = getDriver().findElement(By.xpath("//*[@id='source-code-management']")).getText();
         Assert.assertEquals(sourceCodeManagementBlock, "Source Code Management");
 
+    }
+
+    @Test
+    public void testCreateNewItemFPTU () {
+        TestUtils.createNewItem(this,PROJECT_NAME, TestUtils.Item.FREESTYLE_PROJECT);
+        getDriver().findElement(By.name("Submit")).click();
+
+        String configurationHeaderH1 = getDriver().findElement(By.tagName("h1")).getText();
+        Assert.assertEquals(configurationHeaderH1, PROJECT_NAME);
     }
 }
