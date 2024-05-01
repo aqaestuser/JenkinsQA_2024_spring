7376353971932e72753a3dc55856260df780db75
KathyGU <97336394+KathyGU@users.noreply.github.com>
2024-05-01 09:39:12 +0300

    TC_09.002.02: verify section names of security block (#1159)
    * TC_09.002.02: verify section names of security block
    
    * ArrayList for sections's names added
    
    * getWait2() added
    
    * In-process Script Approval section deleted
    
    * Jenkins interface updated

--- src/test/java/school/redrover/ManageJenkinsTest.java
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -1,9 +1,15 @@
 package school.redrover;
 
+import org.checkerframework.checker.signature.qual.DotSeparatedIdentifiersOrPrimitiveType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageJenkinsTest extends BaseTest {
 
@@ -14,5 +20,20 @@ public class ManageJenkinsTest extends BaseTest {
 
         String pageTitle = getDriver().getTitle().split(" ")[0];
         Assert.assertEquals(pageTitle, "Security");
+    }
+
+    @Test
+    public void testSectionNamesOfSecurityBlock() {
+        final List <String> sectionsNamesExpected = List.of("Security", "Credentials", "Credential Providers",
+                "Users");
+
+        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
+
+        List <WebElement> securityBlockElements = getDriver().findElements(By
+                .xpath("//section[contains(@class, 'jenkins-section')][2]//div//dt"));
+
+        for (int i = 0; i < securityBlockElements.size(); i++) {
+            Assert.assertTrue(securityBlockElements.get(i).getText().matches(sectionsNamesExpected.get(i)));
+        }
     }
 }
