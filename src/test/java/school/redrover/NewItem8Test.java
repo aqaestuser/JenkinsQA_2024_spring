49e44dc407e1ea51448a7beee4b5f5ef25d8d140
Yaroslav-R-P <75783575+Yaroslav-R-P@users.noreply.github.com>
2024-05-01 09:38:33 +0300

    Jp/refactoring new item8 test class (#1160)
    * RF _Refactoring NewItem8Test Class
    
    * RF_00.005.11 Refactoring NewItem8Test Class

--- src/test/java/school/redrover/NewItem8Test.java
+++ src/test/java/school/redrover/NewItem8Test.java
@@ -7,25 +7,41 @@ import school.redrover.runner.BaseTest;
 
 public class NewItem8Test extends BaseTest {
 
-    private final String ITEM_NAME = "My first Multibranch Pipeline";
+    private final String FIRST_ITEM_NAME = "My first Multibranch Pipeline";
+    private final String SECOND_ITEM_NAME = "My second Multibranch Pipeline";
+
+    public void createItem() {
+        enterToNewItemPage();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(FIRST_ITEM_NAME);
+        getDriver().findElement(By.xpath("//label[.='Multibranch Pipeline']")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    public void enterToNewItemPage() {
+        getDriver().findElement(By.className("task")).click();
+    }
+
+    public void goToHomePage() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
 
     @Test
     public void testCreateItem() {
         getDriver().findElement(By.className("task")).click();
-        getDriver().findElement(By.className("jenkins-input")).sendKeys(ITEM_NAME);
-        getDriver().findElement(By.xpath("(//li[@tabindex='0'])[5]")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(FIRST_ITEM_NAME);
+        getDriver().findElement(By.xpath("//label[.='Multibranch Pipeline']")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.linkText(ITEM_NAME)).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.linkText(FIRST_ITEM_NAME)).isDisplayed());
     }
 
     @Test
     public void testCreateItemFromNewJob() {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.className("jenkins-input")).sendKeys(ITEM_NAME);
-        getDriver().findElement(By.xpath("(//li[@tabindex='0'])[5]")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(FIRST_ITEM_NAME);
+        getDriver().findElement(By.xpath("//label[.='Multibranch Pipeline']")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.linkText(ITEM_NAME)).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.linkText(FIRST_ITEM_NAME)).isDisplayed());
     }
 }
