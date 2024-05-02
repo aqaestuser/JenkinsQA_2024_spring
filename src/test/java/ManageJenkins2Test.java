954d2701e21f5c3b4d7856de19e4baf74b5f94f1
Yaroslav-R-P <75783575+Yaroslav-R-P@users.noreply.github.com>
2024-05-02 11:44:45 +0300

    Jp/manage jenkins (#1182)
    * RF _Refactoring NewItem8Test Class
    
    * TC_09.001.03 | Manage Jenkins See the settings search field in the upper right corner of "Manage Jenkins" module
    
    * TC_09.001.03 | Manage Jenkins See the settings search field in the upper right corner of "Manage Jenkins" module

---
+++ src/test/java/ManageJenkins2Test.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class ManageJenkins2Test extends BaseTest {
+
+    private void goToManageJenkins() {
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+    }
+
+    @Test
+    public void testUserSeeTheSearchSettingsField() {
+        goToManageJenkins();
+
+        Assert.assertTrue(getDriver().findElement(By.className("jenkins-search__input")).isDisplayed());
+    }
+}
