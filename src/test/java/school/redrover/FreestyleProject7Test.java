6ce7653cf3b3c98de511f00622f662f8d53b7056
Nadya Shulga <118784278+NadyaShulga@users.noreply.github.com>
2024-05-05 16:51:57 -0400

    Ns/refactoring freestyle project7 test (#1247)
    * Refactoring FreestyleProject7Test
    
    * refactoring FreestyleProject7Test
    
    * refactoring FreestyleProject7Test

--- src/test/java/school/redrover/FreestyleProject7Test.java
+++ src/test/java/school/redrover/FreestyleProject7Test.java
@@ -1,38 +1,23 @@
 package school.redrover;
-
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
+import java.util.List;
 
 public class FreestyleProject7Test extends BaseTest {
-
-    private void createFreestyleProject(String freestyleProjectName) {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.name("name")).sendKeys(freestyleProjectName);
-        getDriver().findElement(By.xpath("//label/span[text() ='Freestyle project']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
-    }
-
-    private void openDashboard(){
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
-    }
-
+    final String freestyleProjectName = "FreestyleProjectTest";
     @Test
-    public void testNewCreateFreestyleProject() {
-        final String expectedFreestyleProjectName = "FreestyleProjectTest";
-        final String expectedStateMessage = "Permalinks";
+    public void testCreateFreestyleProject() {
 
-        createFreestyleProject(expectedFreestyleProjectName);
-        openDashboard();
+        List<String> itemList = new HomePage(getDriver())
+                .clickNewItem()
+                .setItemName(freestyleProjectName)
+                .selectFreestyleAndClickOk()
+                .clickSave()
+                .clickLogo()
+                .getItemList();
 
-        getDriver().findElement(By.cssSelector("a[class='jenkins-table__link model-link inside'] span")).click();
-
-        final String actualFreestyleProjectName = getDriver().findElement(By.xpath("//h1")).getText();
-        final String actualStateMessage = getDriver().findElement(By.xpath("//div[@id='main-panel']/h2")).getText();
-
-        Assert.assertEquals(actualFreestyleProjectName, expectedFreestyleProjectName);
-        Assert.assertEquals(actualStateMessage, expectedStateMessage);
+        Assert.assertTrue(itemList.contains(freestyleProjectName));
     }
 }
