57c544cee0750ab50d33c200525780c34d311072
Oleg Veremei <veremeioleg@gmail.com>
2024-05-07 13:53:01 +0300

    resolved conflicts

--- src/test/java/school/redrover/model/PipelinePage.java
+++ src/test/java/school/redrover/model/PipelinePage.java
@@ -187,6 +187,7 @@ public class PipelinePage extends BasePage {
 
     public boolean isBtnPresentInSidebar(String btnText) {
         getWait2().until(ExpectedConditions.visibilityOfAllElements(taskLinkTextElements));
+
         return taskLinkTextElements.stream()
                 .anyMatch(element -> btnText.equals(element.getText()));
     }
