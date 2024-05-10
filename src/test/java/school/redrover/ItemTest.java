fe3b982c743c41dc39b90f67dd15a2a5897a6854
Evgeniy <119495875+Gaztechica@users.noreply.github.com>
2024-05-10 13:26:55 +0400

    refactor test POM, delete copy (#1371)

--- src/test/java/school/redrover/ItemTest.java
+++ src/test/java/school/redrover/ItemTest.java
@@ -11,21 +11,7 @@ import java.util.List;
 
 public class ItemTest extends BaseTest {
 
-    @Test
-    public void testElementPeople() {
-        new ItemPage(getDriver())
-                .ElementPeopleClick();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='jenkins-app-bar__content']")).getText(), "People");
-    }
-
-    @Test
-    public void testElementWelcome() {
-        new ItemPage(getDriver())
-                .ElementWelcomeClic();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(.,'Welcome to Jenkins!')]")).getText(), "Welcome to Jenkins!");
-    }
 
     @Test
     public void testAddItem() {
@@ -41,27 +27,4 @@ public class ItemTest extends BaseTest {
 
     }
 
-    @DataProvider(name = "userAddProvider")
-    public Object[][] userAddProvider() {
-        return  new Object[][] {
-                {"Gleb", "Password", "Full name", "E-mail@com"},
-                {"Ignat", "Password", "Full name", "E-mail@com"},
-                {"Lion", "Password", "Full name", "E-mail@com"},
-                {"Oleg", "Password", "Full name", "E-mail@com"},
-                {"Den", "Password", "Full name", "E-mail@com"},
-        };
-    }
-
-    @Test(dataProvider = "userAddProvider")
-    public void testCheckPeople(String Username, String Password, String Fullname, String Email) {
-
-        List<String> showNam = new HomePage(getDriver())
-                .clickManageJenkins()
-                .clickUsers()
-                .createUser(Username, Password, Fullname, Email)
-                .getUserIDList();
-//        System.out.printf(showNam.toString());
-
-        Assert.assertTrue(showNam.contains(Username));
-    }
 }
