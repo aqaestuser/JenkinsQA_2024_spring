b77d056888ab27fb9e88c282c6c4e4afc97f2160
Dr.Johanson <doc.johanson@gmail.com>
2024-05-11 13:44:01 +0500

    RF_09.005.04 ManageAppearance1Test POM RF

--- src/test/java/school/redrover/ManageAppearance1Test.java
+++ src/test/java/school/redrover/ManageAppearance1Test.java
@@ -1,50 +1,45 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
 
 public class ManageAppearance1Test extends BaseTest {
 
-    private void goToAppearance() {
-        getDriver().findElement(By.cssSelector("[href='/manage']")).click();
-        getDriver().findElement(By.cssSelector("[href='appearance']")).click();
-    }
-
-    private void clickSaveButton() {
-        getDriver().findElement(By.name("Submit"));
-    }
-
-    private String getThemeAttribute() {
-        return getDriver().findElement(By.cssSelector("html[data-theme]"))
-                .getAttribute("data-theme");
-    }
-
     @Test
     public void testDarkThemeApply() {
-        goToAppearance();
-        getDriver().findElement(By.cssSelector("[for='radio-block-0']")).click();
-        clickSaveButton();
+        final String actualThemeApplied = new HomePage(getDriver())
+                .clickManageJenkins()
+                .clickAppearanceButton()
+                .clickDarkThemeButton()
+                .clickApplyButton()
+                .getCurrentThemeAttribute();
 
-        Assert.assertEquals(getThemeAttribute(), "dark");
+        Assert.assertEquals(actualThemeApplied, "dark");
     }
 
     @Test
     public void testDefaultThemeApply() {
-        goToAppearance();
-        getDriver().findElement(By.cssSelector("[for='radio-block-2']")).click();
-        clickSaveButton();
+        final String actualThemeApplied = new HomePage(getDriver())
+                .clickManageJenkins()
+                .clickAppearanceButton()
+                .clickDefaultThemeButton()
+                .clickApplyButton()
+                .getCurrentThemeAttribute();
 
-        Assert.assertEquals(getThemeAttribute(), "none");
+        Assert.assertEquals(actualThemeApplied, "none");
     }
 
     @Test
     public void testSystemThemeApply() {
-        goToAppearance();
-        getDriver().findElement(By.cssSelector("[for='radio-block-1']")).click();
-        clickSaveButton();
+        final String actualThemeApplied = new HomePage(getDriver())
+                .clickManageJenkins()
+                .clickAppearanceButton()
+                .clickSystemThemeButton()
+                .clickApplyButton()
+                .getCurrentThemeAttribute();
 
-        Assert.assertTrue(getThemeAttribute().contains("system"));
+        Assert.assertTrue(actualThemeApplied.contains("system"));
     }
 }
