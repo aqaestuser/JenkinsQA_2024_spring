ac42b27bb39c9dfc7fd53b2a540a9ef7c18bbfbb
Roman Poptsov <82845376+PRoman-86@users.noreply.github.com>
2024-05-12 22:34:51 +0300

    RF all tests in class AItemTest to POM (#1421)

--- src/test/java/school/redrover/AItemTest.java
+++ src/test/java/school/redrover/AItemTest.java
@@ -1,95 +1,85 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.CreateNewItemPage;
 import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.TestUtils;
+import java.util.List;
 
 public class AItemTest extends BaseTest {
 
-    private final By nameUpItem = By.xpath("//span[.='New Folder']");
-    private final By renameTextField = By.xpath("//input[@name='newName']");
-    private Actions actions;
-
-    private Actions getActions() {
-        if (this.actions == null) this.actions = new Actions(getDriver());
-        return this.actions;
-    }
+    private final String NAME = "Name";
+    private final String NEW_NAME = "New Name";
+    private final String ITEM_NAME_HINT_TEXT = "» This field cannot be empty, please enter a valid name";
+    private List<String> itemsList = null;
 
     @Test
     public void testCreateItemEmptyNameNegative() {
         new HomePage(getDriver())
                 .clickNewItem()
-                .setItemName("New Item")
+                .setItemName(NAME)
                 .clearItemNameField()
                 .selectMultibranchPipelineAndClickOk();
 
         Assert.assertFalse(new CreateNewItemPage(getDriver()).okButtonIsEnabled());
     }
 
-    @Ignore
     @Test
     public void testCreateNewFolder() {
-        TestUtils.createItem(TestUtils.FOLDER, "New Folder", this);
-        TestUtils.goToMainPage(getDriver());
+        itemsList = new HomePage(getDriver())
+                .clickNewItem()
+                .setItemName(NAME)
+                .selectFolderAndClickOk()
+                .clickSaveButton()
+                .clickLogo()
+                .getItemList();
 
-        Assert.assertTrue(getDriver().findElement(nameUpItem).isDisplayed());
+        Assert.assertTrue(itemsList.contains(NAME));
     }
 
-    @Ignore
     @Test
     public void testRenameFolder() {
-        getActions().moveToElement(getDriver().findElement(nameUpItem)).perform();
-        getDriver().findElement(By.linkText("New Folder")).click();
-        getDriver().findElement(By.xpath("//a[normalize-space()='Rename']")).click();
-        getDriver().findElement(renameTextField).clear();
-        getDriver().findElement(renameTextField).sendKeys("New Name");
-        getDriver().findElement(By.name("Submit")).click();
+        itemsList = new HomePage(getDriver())
+                .clickNewItem()
+                .setItemName(NAME)
+                .selectFolderAndClickOk()
+                .clickSaveButton()
+                .clickLogo()
+                .openItemDropdownWithSelenium(NAME)
+                .renameFolderFromDropdown()
+                .setNewName(NEW_NAME)
+                .clickRename()
+                .clickLogo()
+                .getItemList();
 
-        Assert.assertTrue(getDriver().findElement(By.xpath("//h1[contains(.,'New Name')]")).isDisplayed());
+        Assert.assertTrue(itemsList.contains(NEW_NAME));
     }
 
-    @Ignore
-    @Test
-    public void testRenameFolderShort() {
-        TestUtils.renameItem(this, "New Name", "Renamed Folder");
-        Assert.assertTrue(getDriver().findElement(By.xpath("//h1[contains(.,'Renamed Folder')]")).isDisplayed());
-    }
-
-    @Ignore
     @Test
     public void testCreateMulticonfigurationProjectNegative() {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.xpath("//span[text()='" + TestUtils.MULTI_CONFIGURATION_PROJECT + "']")).click();
+        new HomePage(getDriver())
+                .clickNewItem()
+                .selectMultiConfigurationAndClickOk();
 
-        Assert.assertEquals(getDriver().findElement
-                (By.id("itemname-required")).getText(), "» This field cannot be empty, please enter a valid name");
+        Assert.assertEquals(new CreateNewItemPage(getDriver()).getItemNameHintText(), ITEM_NAME_HINT_TEXT);
     }
 
-    @Ignore
     @Test
     public void testCreateMulticonfigurationProject() {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("name"))).sendKeys("New Folder");
-        getDriver().findElement(By.xpath("//span[text()='" + TestUtils.MULTI_CONFIGURATION_PROJECT + "']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+        itemsList = new HomePage(getDriver())
+                .clickNewItem()
+                .setItemName(NAME)
+                .selectMultiConfigurationAndClickOk()
+                .clickSaveButton()
+                .clickLogo()
+                .getItemList();
 
-        Assert.assertTrue(getDriver().findElement(By.xpath("//h1[normalize-space()='Configure']")).isDisplayed());
-        TestUtils.goToMainPage(getDriver());
-        Assert.assertTrue(getDriver().findElement(nameUpItem).isDisplayed());
+        Assert.assertTrue(itemsList.contains(NAME));
     }
 
-    @Ignore
     @Test
     public void testFooterRestApiLinkRGB() {
-        WebElement restApi = getDriver().findElement(By.xpath("//a[normalize-space()='REST API']"));
-        Assert.assertEquals(restApi.getCssValue("color"), "rgba(20, 20, 31, 1)");
+        Assert.assertEquals(new HomePage(getDriver()).getRestApiLinkColor(), "rgba(20, 20, 31, 1)");
     }
 }
