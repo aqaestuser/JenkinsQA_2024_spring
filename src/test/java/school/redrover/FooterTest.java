692b2b3f0e925d4b6d8ef42939daec3ee1b0296e
OlesyaNay <110473421+OlesyaNay@users.noreply.github.com>
2024-05-12 20:16:02 -0300

    Remove duplicate test (testApiInfo) in FooterTest class

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,7 +1,5 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.model.AboutJenkinsPage;
@@ -11,13 +9,6 @@ import school.redrover.runner.BaseTest;
 import java.util.List;
 
 public class FooterTest extends BaseTest {
-
-    @Test
-    public void testApiInfo() {
-        getDriver().findElement(By.xpath("//*[@class='jenkins-button jenkins-button--tertiary rest-api']")).click();
-
-        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/api/");
-    }
 
     @Test
     public void testLinkButtonsListInVersionDropDown() {
