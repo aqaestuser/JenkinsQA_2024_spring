0a4d09b930392574b10cb5252bb25213cc9adb14
Dr.Johanson <doc.johanson@gmail.com>
2024-05-12 21:14:12 +0500

    RF_15.002.01 remove unused import

--- src/test/java/school/redrover/NodesTest.java
+++ src/test/java/school/redrover/NodesTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
