4c60b2940f85f808ca310f5f4f2e79ad97642f61
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-06-02 20:51:11 +0300

    [+] seven goes

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -9,7 +9,7 @@ import school.redrover.runner.BaseTest;
 public class HeaderTest extends BaseTest {
 
     @Test
-    public void testTooltipAccessiblebccddddccbbModed() {
+    public void testTooltipAccessiblebcoded() {
         String warningTooltipText = new HomePage(getDriver())
                 .getHeader().clickWarningIcon()
                 .getHeader().getWarningTooltipText();
