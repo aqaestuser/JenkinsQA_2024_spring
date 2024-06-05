d4227a39b3d3e13c1dd68f171632168264e45be7
Anastasia Biletska <114147000+biletskaanastasiiia@users.noreply.github.com>
2024-06-05 19:31:31 +0300

    HeaderTest - Review: Remove Duplicate Tests. RF to POM. RF Ignored Tests (#1604)

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -1,5 +1,8 @@
 package school.redrover;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -7,31 +10,44 @@ import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
 
 public class HeaderTest extends BaseTest {
+    @Epic("14_Header")
 
     @Test
+    @Story("US_14.004 Warning item")
+    @Description("Check tooltip with the list of problems")
     public void testTooltipAccessible() {
         String warningTooltipText = new HomePage(getDriver())
-                .getHeader().clickWarningIcon()
-                .getHeader().getWarningTooltipText();
+                .getHeader()
+                .clickWarningIcon()
+                .getHeader()
+                .getWarningTooltipText();
 
         Assert.assertTrue(warningTooltipText.contains("Warnings"));
     }
 
     @Test
+    @Story("US_14.004 Warning item")
+    @Description("Check getting the settings page with warnings")
     public void testWarningsSettingPage() {
         String pageTitle = new HomePage(getDriver())
-                .getHeader().clickWarningIcon()
-                .getHeader().clickConfigureTooltipButton()
+                .getHeader()
+                .clickWarningIcon()
+                .getHeader()
+                .clickConfigureTooltipButton()
                 .getTitleText();
 
         Assert.assertTrue(pageTitle.contains("Security"));
     }
 
     @Test
+    @Story("US_14.004 Warning item")
+    @Description("Check opening a settings page by clicking on link “Manage Jenkins")
     public void testAccessToManageJenkinsPage() {
         String pageTitle = new HomePage(getDriver())
-                .getHeader().clickWarningIcon()
-                .getHeader().clickManageJenkinsTooltipLink()
+                .getHeader()
+                .clickWarningIcon()
+                .getHeader()
+                .clickManageJenkinsTooltipLink()
                 .getPageHeadingText();
 
         Assert.assertTrue(pageTitle.contains("Manage Jenkins"));
@@ -39,9 +55,12 @@ public class HeaderTest extends BaseTest {
     }
 
     @Test
+    @Story("US_14.005 Entrance to the user's account")
+    @Description("Check user logout")
     public void testLogout() {
         String actualPageTitle = new HomePage(getDriver())
-                .getHeader().clickLogOut()
+                .getHeader()
+                .clickLogOut()
                 .getSignInToJenkinsTitle();
 
         Assert.assertEquals(actualPageTitle, "Sign in to Jenkins");
