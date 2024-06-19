eebad67616b3947993a70e02cc6411cebe0cd21d
Kate <katrinna85@gmail.com>
2024-06-19 14:48:45 -0400

    Refactor Views class and add Steps

--- src/test/java/school/redrover/ViewsTest.java
+++ src/test/java/school/redrover/ViewsTest.java
@@ -6,14 +6,107 @@ import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.model.HomePage;
+import school.redrover.model.ViewMyListConfigPage;
 import school.redrover.model.ViewPage;
 import school.redrover.runner.BaseTest;
+
 
 import java.util.List;
 
 public class ViewsTest extends BaseTest {
 
     private static final String MY_VIEW_NAME = "EmployeeView";
+    private static final String VIEW_NAME = "in progress";
+    private static final String VISIBLE = "visible";
+
+    @Test
+    public void testGoToMyViewsFromUsernameDropdown() {
+        String views = "My Views";
+
+        boolean textVisibility = new HomePage(getDriver())
+                .getHeader().clickMyViewsOnHeaderDropdown()
+                .isThereTextInBreadcrumbs(views);
+
+        Assert.assertTrue(textVisibility, "'My Views' didn't open");
+    }
+
+    @Test
+    public void testDisplayViewWithListViewConstraints() {
+        final String invisible = "invisible";
+
+        List<String> projectNameList = new HomePage(getDriver())
+                .clickNewItem()
+                .typeItemName(VISIBLE)
+                .selectFolderAndClickOk()
+                .clickLogo()
+                .clickNewItem()
+                .typeItemName(invisible)
+                .selectPipelineAndClickOk()
+                .clickLogo()
+                .clickPlusToCreateView()
+                .setViewName(VIEW_NAME)
+                .clickListViewRadioButton()
+                .clickCreateViewButton()
+                .clickProjectName(VISIBLE)
+                .clickOkButton()
+                .getProjectNames();
+
+        List<String> expectedProjectNameList = List.of(VISIBLE);
+        int expectedProjectListSize = 1;
+
+        Assert.assertTrue(projectNameList.size() == expectedProjectListSize
+                        && projectNameList.equals(expectedProjectNameList),
+                "Error displaying projects in View");
+    }
+
+    public void createView(String viewName) {
+
+        new HomePage(getDriver())
+                .clickPlusToCreateView()
+                .setViewName(viewName)
+                .clickListViewRadioButton()
+                .clickCreateViewButton();
+    }
+
+
+    @Test
+    public void testAddColumnIntoListView() {
+
+        new HomePage(getDriver())
+                .clickCreateAJob()
+                .typeItemName(VISIBLE)
+                .selectFolderAndClickOk()
+                .clickSaveButton()
+                .clickLogo();
+
+        createView(VIEW_NAME);
+
+        int numberOfColumns = new ViewMyListConfigPage(getDriver())
+                .clickProjectName(VISIBLE)
+                .clickAddColumn()
+                .clickColumnName()
+                .clickOkButton()
+                .clickLogo()
+                .clickViewName(VIEW_NAME)
+                .getSizeColumnList();
+
+        Assert.assertEquals(numberOfColumns, 7, "Description column is not added");
+    }
+
+    @Test(dependsOnMethods = "testAddColumnIntoListView")
+    public void testChangeOrderOfColumns() {
+
+        List<String> columnNameText = new HomePage(getDriver())
+                .clickViewName(VIEW_NAME)
+                .clickEditViewOnSidebar()
+                .scrollIntoSubmit()
+                .moveDescriptionToStatusColumn()
+                .getColumnNameText();
+
+        Assert.assertEquals(columnNameText.get(0), "Description");
+    }
+
+
 
     @Test
     @Story("US_10.001 Create View")
