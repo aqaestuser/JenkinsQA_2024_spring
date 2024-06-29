6b1c95c0c00c10628689801fd813f72485c9aa62
Serhii Malko <malsv@ukr.net>
2024-06-30 01:32:21 +0300

    add methods to AsserUtils (#1705)

--- src/test/java/school/redrover/runner/AssertUtils.java
+++ src/test/java/school/redrover/runner/AssertUtils.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import java.util.List;
 
 public final class AssertUtils {
-    private static final String PREFIX = "Expected results:";
+    private static final String PREFIX = "Expected results: ";
 
     public static AssertUtils allureAnnotation(String expectedResults) {
         Allure.step(String.format("%s %s", PREFIX, expectedResults));
@@ -26,6 +26,14 @@ public final class AssertUtils {
         Assert.assertEquals(actual, expected, errorMessage);
     }
 
+    public <T> void notEquals(T actual, T expected) {
+        Assert.assertNotEquals(actual, expected);
+    }
+
+    public <T> void notEquals(T actual, T expected, String errorMessage) {
+        Assert.assertNotEquals(actual, expected, errorMessage);
+    }
+
     public void isTrue(boolean condition) {
         Assert.assertTrue(condition);
     }
@@ -34,8 +42,35 @@ public final class AssertUtils {
         Assert.assertTrue(condition, errorMessage);
     }
 
+    public void isFalse(boolean condition) {
+        Assert.assertFalse(condition);
+    }
+
+    public void isFalse(boolean condition, String errorMessage) {
+        Assert.assertFalse(condition, errorMessage);
+    }
+
+    public void isNull(Object object) {
+        Assert.assertNull(object);
+    }
+
+    public void isNull(Object object, String errorMessage) {
+        Assert.assertNull(object, errorMessage);
+    }
+
+    public void notNull(Object object) {
+        Assert.assertNotNull(object);
+    }
+
+    public void notNull(Object object, String errorMessage) {
+        Assert.assertNotNull(object, errorMessage);
+    }
+
     public <T> void listContainsObject(List<T> list, T object, String errorMessage) {
         Assert.assertListContainsObject(list, object, errorMessage);
     }
 
+    public <T> void listNotContainsObject(List<T> list, T object, String errorMessage) {
+        Assert.assertListNotContainsObject(list, object, errorMessage);
+    }
 }
